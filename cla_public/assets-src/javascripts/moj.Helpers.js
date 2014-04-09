9a1d40428190c1d272704c0a8fcfc93bc46ad537
Dom Smith <dom@teneightfive.com>
2014-04-09 12:47:03 +0100

    Refactor form filler helper to also fill apply form

--- cla_public/assets-src/javascripts/moj.Helpers.js
+++ cla_public/assets-src/javascripts/moj.Helpers.js
@@ -36,4 +36,11 @@
   Handlebars.registerHelper('formatCurrency', function(value) {
     return parseFloat(value).toFixed(2);
   });
+  Handlebars.registerHelper('if_eq', function(a, b, opts) {
+    if(a === b) {
+      return opts.fn(this);
+    } else {
+      return opts.inverse(this);
+    }
+  });
 })();
