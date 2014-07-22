87999c3a71a2ca33ca007e81a4d12d0982528867
Peter Idah <peter.idah@gmail.com>
2014-07-22 18:42:08 +0100

    exit on failure

--- docker/build.sh
+++ docker/build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -e
 CLA_DOCKER_REGISTRY=docker.cla.dsd.io
 sudo docker build --force-rm=true -t $CLA_DOCKER_REGISTRY/$APP_NAME .
 sudo docker tag $CLA_DOCKER_REGISTRY/$APP_NAME $CLA_DOCKER_REGISTRY/$APP_NAME:$GIT_COMMIT 
