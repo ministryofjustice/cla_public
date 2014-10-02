47f14592fe52782668bbd30f8058df28cd3494cc
Peter Idah <peter.idah@gmail.com>
2014-10-02 17:39:10 +0100

    cutover to s3-backed docker registry

--- docker/build.sh
+++ docker/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
-CLA_DOCKER_REGISTRY=docker.cla.dsd.io
+CLA_DOCKER_REGISTRY=cla-registry.dsd.io
 
-[[ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://$CLA_DOCKER_REGISTRY/v1/repositories/cla_frontend/images) == '200' ]] || { echo -e "\nTHE DOCKER REGISTRY IS DOWN!"; exit 1; }
+[[ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' https://$CLA_DOCKER_REGISTRY/v1/repositories/$APP_NAME/images) == '200' ]] || { echo -e "\n$(tput setaf 1)THE DOCKER REGISTRY IS DOWN!$(tput sgr0)"; exit 1; }
 
 sudo docker build --force-rm=true -t $CLA_DOCKER_REGISTRY/$APP_NAME .
 sudo docker tag $CLA_DOCKER_REGISTRY/$APP_NAME $CLA_DOCKER_REGISTRY/$APP_NAME:$GIT_COMMIT 
