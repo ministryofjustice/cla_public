cae6e068da38db4b5f4f892a53ca8163f9965068
Tyom Semonov <inspiro@gmail.com>
2014-10-29 13:54:40 +0000

    Update port for proxied server

--- tasks/serve.js
+++ tasks/serve.js
@@ -8,7 +8,7 @@ var argv = require('yargs').argv;
 // Proxy existing server via brower-sync and serve on localhost:3000
 gulp.task('serve', function() {
   var host = argv.host || argv.h || 'localhost';
-  var port = argv.port || argv.p || 8003;
+  var port = argv.port || argv.p || 5000;
 
   browserSync({
     proxy: host + ':' + port,
