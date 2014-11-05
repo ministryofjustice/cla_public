b4808f36a2d678975f0ca086acb636eb6a245bf7
Andy Driver <andy.driver@digital.justice.gov.uk>
2014-11-05 16:29:03 +0000

    Change settings to Python files to allow for per environment dependencies and app config

---
+++ cla_public/config/base.py
@@ -0,0 +1,14 @@
+DEBUG = False
+
+TESTING = False
+
+SECRET_KEY = "e\x1bU3\xbc\x00\xb0\xae\x07z~D\x03\xcc'\x04\x1f\xcd\xe8\xee\x83\xe6\x9b\x19"
+
+SESSION_COOKIE_SECURE = True
+
+APP_SETTINGS = {
+    'app_title': 'Civil Legal Aid',
+    'proposition_title': 'Civil Legal Aid'
+}
+
+EXTENSIONS = []
