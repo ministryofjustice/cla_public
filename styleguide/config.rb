5cef54c80eeccf6d1ae5f06d8db005314ff7a43f
Tyom Semonov <inspiro@gmail.com>
2014-11-07 11:37:04 +0000

    fixup! Add initial styleguide components (Sculptor)

--- styleguide/config.rb
+++ styleguide/config.rb
@@ -20,12 +20,12 @@ page '*.json', :layout => false
 # end
 
 ready do
-  # Create mappings for standalone (implementation) pages (iframe embeddable)
+  # Create mappings for isolated component pages (iframe embeddable)
   resources_for('/', exclude_indexes: true, allow_hidden: true).each do |r|
-    proxy "#{r.path.sub(r.ext, '')}-standalone.html", r.path, layout: 'standalone'
+    proxy "#{r.path.sub(r.ext, '')}-isolated.html", r.path, layout: 'isolated'
   end
   resources_for('/', allow_hidden: true).each do |r|
-    proxy "#{r.path.sub(r.ext, '')}/standalone", r.path, layout: 'standalone'
+    proxy "#{r.path.sub(r.ext, '')}/isolated", r.path, layout: 'isolated'
   end
 end
 
