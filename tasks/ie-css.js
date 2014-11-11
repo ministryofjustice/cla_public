3d890bed0ade1b37d11ca064f7effaf52cddd6d5
Tyom Semonov <inspiro@gmail.com>
2014-11-11 16:37:56 +0000

    Fix Guild IE CSS loadPath

--- tasks/ie-css.js
+++ tasks/ie-css.js
@@ -12,7 +12,8 @@ gulp.task('ie-css', ['clean-iecss'], function() {
     .src(paths.ie_styles)
     .pipe(sass({
       lineNumbers: true,
-      style: 'compact'
+      style: 'compact',
+      loadPath: 'node_modules/govuk_frontend_toolkit/' // add node module toolkit path
     }))
     .on('error', function (err) { console.log(err.message); })
     .pipe(gulp.dest(paths.dest + 'ie-stylesheets/'))
