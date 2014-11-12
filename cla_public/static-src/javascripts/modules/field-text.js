8d397b44a6eadbf13693ad0ca77850d2a24e8aa7
Tyom Semonov <inspiro@gmail.com>
2014-11-12 17:16:57 +0000

    FE: Implement JS expandable more info elements

--- cla_public/static-src/javascripts/modules/field-text.js
+++ cla_public/static-src/javascripts/modules/field-text.js
@@ -2,6 +2,31 @@
   'use strict';
 
   moj.Modules.FieldHelp = {
+    moreInfos: [],
 
+    init: function() {
+      this.cacheEls();
+      this.bindEvents();
+      this.addMoreInfoLink();
+    },
+
+    bindEvents: function() {
+      this.moreInfos.parent()
+        .on('click', 'a', this.handleToggle);
+    },
+
+    handleToggle: function(evt) {
+      $(evt.target).parent().toggleClass('s-expanded');
+    },
+
+    addMoreInfoLink: function() {
+      $.each(this.moreInfos, function(i, el) {
+        $('<a class="field-more-info-toggle">more info</a>').insertBefore($(el));
+      });
+    },
+
+    cacheEls: function() {
+      this.moreInfos = $('.field-more-info');
+    }
   };
 }());
