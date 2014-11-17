bdb397f0389acf87e104a05191452a624e645654
Tyom Semonov <inspiro@gmail.com>
2014-11-17 17:53:08 +0000

    Revert "BE: Add `split` Jinja filter"
    This reverts commit 2589d82f90a16a368009c008a93a46cbd68c991b.

--- cla_public/apps/base/filters.py
+++ cla_public/apps/base/filters.py
@@ -1,10 +1,5 @@
-import re
 from cla_public.apps.base import base
 
 @base.app_template_filter()
 def test(value):
     return value
-
-@base.app_template_filter()
-def split(value, pattern):
-    return re.split(pattern, value)
