f0ae07a063383516a6982b8e6a6cc1ab2ae3da80
Josh Rowe <josh@s-block.com>
2014-11-19 15:16:01 +0000

    redirect to ineligible if api returns ineligible

--- cla_public/apps/checker/api.py
+++ cla_public/apps/checker/api.py
@@ -111,6 +111,14 @@ def post_to_eligibility_check_api(form):
         backend.eligibility_check(reference).patch(payload)
 
 
+def post_to_is_eligible_api():
+    backend = get_api_connection()
+    reference = session.get('eligibility_check')
+
+    if reference:
+        return backend.eligibility_check(reference).is_eligible().post().get('is_eligible')
+
+
 def post_to_case_api(form):
     backend = get_api_connection()
     reference = session.get('eligibility_check')
