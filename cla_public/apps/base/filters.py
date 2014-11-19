a895661f7ce1a814f308dbe8b5836a4cdb472c8c
Tyom Semonov <inspiro@gmail.com>
2014-11-19 15:08:47 +0000

    Revert "BE: Update custom template filter"
    This reverts commit ea0c0beb1d2aa0d5970b629ac06e6f9b9708bfdd.

--- cla_public/apps/base/filters.py
+++ cla_public/apps/base/filters.py
@@ -1,6 +1,5 @@
-import re
 from cla_public.apps.base import base
 
 @base.app_template_filter()
-def matches(value, pattern):
-    return bool(re.search(pattern, value))
+def test(value):
+    return value
