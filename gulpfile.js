19d0a2e3ea9f060932f6f3c629d7e746a183bfae
Tyom Semonov <inspiro@gmail.com>
2014-11-27 14:45:27 +0000

    GULP: Remove duplicate task from main Gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -6,20 +6,6 @@ var gulp = require('gulp');
 var requireDir = require('require-dir');
 var dir = requireDir('./tasks');
 
-var plugins = require('gulp-load-plugins')();
-
-// js templates
-gulp.task('templates', function(){
-  gulp.src('cla_public/static-src/javascripts/templates/*.hbs')
-    .pipe(plugins.handlebars())
-    .pipe(plugins.defineModule('plain'))
-    .pipe(plugins.declare({
-      namespace: 'CLA.templates'
-    }))
-    .pipe(plugins.concat('templates.js'))
-    .pipe(gulp.dest('cla_public/static-src/javascripts'));
-});
-
 gulp.task('build', [
     'minify-css',
     'templates',
