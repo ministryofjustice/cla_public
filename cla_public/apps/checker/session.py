e015d587bbf0184adf6b5ab951ba5130ebc5138f
Josh Rowe <josh@s-block.com>
2014-12-02 17:12:17 +0000

    only display savings, investments and valuables if user has them

--- cla_public/apps/checker/session.py
+++ cla_public/apps/checker/session.py
@@ -34,8 +34,18 @@ class CheckerSession(SecureCookieSession):
 
     @property
     def has_savings(self):
-        return self.get('AboutYouForm_have_savings', NO) == YES \
-               or self.get('AboutYouForm_have_valuables', NO) == YES
+        return self.get('AboutYouForm_have_savings', NO) == YES
+
+    @property
+    def has_valuables(self):
+        return self.get('AboutYouForm_have_valuables', NO) == YES
+
+    @property
+    def has_savings_or_valuables(self):
+        return any([
+            self.has_savings,
+            self.has_valuables,
+        ])
 
     @property
     def owns_property(self):
