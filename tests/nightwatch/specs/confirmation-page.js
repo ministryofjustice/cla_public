47092df46eece5082cc24a0f66f192ad6447e7de
Clive Murray <clive.murray@digital.justice.gov.uk>
2014-12-08 13:57:08 +0000

    WIP: confirmation page callback time tests

--- tests/nightwatch/specs/confirmation-page.js
+++ tests/nightwatch/specs/confirmation-page.js
@@ -28,27 +28,23 @@ var eligibleJourney = function(client) {
     .assert.containsText('h2', 'Request a callback')
     .setValue('input[name="full_name"]', 'John Smith')
     .setValue('input[name="contact_number"]', '01234 567890')
-    .setValue('input[name="contact_number"]', '01234 567890')
     .click('input[name="safe_to_contact"][value="SAFE"]')
     .setValue('input[name="post_code"]', 'E18 1JA')
     .setValue('textarea[name="address"]', '3 Crescent Road\nLondon')
   ;
-  client.getValue('select[name="time_today"]', function(result) {
-    console.log(result.value);
-  });
 };
 
-var checkCallbackTime = function(client, day, time) {
+var checkCallbackTime = function(client, date, time) {
   var then = moment();
-  then.date(day);
+  then.date(date);
   then.hours(time.substr(0, 2));
   then.minutes(time.substr(2, 2));
-  var formattedCallbackTime = then.format('dddd, d MMMM YYYY [at] hh:mm');
+  var formattedCallbackTime = then.format('dddd, D MMMM YYYY [at] HH:mm');
 
   client
-    .pause(2000)
+    .pause(5000)
     .submitForm('form')
-    .pause(10000)
+    .pause(5000)
     .waitForElementVisible('header.confirmation', 2000)
     .assert.containsText('.main-content', formattedCallbackTime)
   ;
@@ -59,15 +55,15 @@ module.exports = {
   'Eligible journey': eligibleJourney,
 
   'Check callback today (next available)': function(client) {
-    var now = new Date();
-    if(now.getDay() !== 0) {
-      if(now.getHours() < 19) {
-        if(now.getDay() === 6 && (now.getHours() > 11 || (now.getHours() === 11 && now.getMinutes() > 14))) {
+    var now = moment();
+    if(now.day() !== 0) {
+      if(now.hour() < 19) {
+        if(now.day() === 6 && (now.hour() > 11 || (now.hour() === 11 && now.minute() > 14))) {
           console.log('Today not available after 11.15am on a Saturday, test skipped');
         } else {
           client.getValue('select[name="time_today"]', function(result) {
-            // TODO: write checkCallbackTime function :)
-            checkCallbackTime(client, now.getDay(), result.value);
+            console.log(result.value);
+            checkCallbackTime(client, now.date(), result.value);
           });
         }
       } else {
@@ -75,6 +71,21 @@ module.exports = {
       }
     } else {
       console.log('Today not available on Sunday, test skipped');
+    }
+  },
+
+  'Eligible journey again': eligibleJourney,
+
+  'Check callback tomorrow': function(client) {
+    client.click('input[name="specific_day"][value="tomorrow"]');
+    var now = moment();
+    if(now.day() !== 6) {
+      client.getValue('select[name="time_tomorrow"]', function(result) {
+        console.log(result.value);
+        checkCallbackTime(client, now.add(1, 'days').date(), result.value);
+      });
+    } else {
+      console.log('Tomorrow not available on Saturday, test skipped');
     }
   },
 
