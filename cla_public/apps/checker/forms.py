9c61d07bd5cacc2f19cb2773d59640a26857ba1a
Josh Rowe <josh@s-block.com>
2014-12-10 15:52:10 +0000

    change help text on property form value field

--- cla_public/apps/checker/forms.py
+++ cla_public/apps/checker/forms.py
@@ -163,7 +163,7 @@ class PropertyForm(NoCsrfForm):
         description=u"Select 'Yes' if you share ownership with a friend, relative or ex-partner")
     property_value = MoneyField(
         u'How much is the property worth?',
-        description=u"Use a property website",
+        description=u"Use a property website or the land registry website",
         validators=[InputRequired(u'Please enter a valid amount'), NumberRange(min=0)])
     mortgage_remaining = MoneyField(
         u'How much is left to pay on the mortgage?',
