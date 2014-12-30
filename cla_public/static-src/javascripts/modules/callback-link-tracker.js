11d3783bd270a5f78cf832954a8541063cf0ac8a
Tyom Semonov <inspiro@gmail.com>
2014-12-30 12:42:03 +0000

    FE: Fix JS linting issues
    Refer to GA as the window property

--- cla_public/static-src/javascripts/modules/callback-link-tracker.js
+++ cla_public/static-src/javascripts/modules/callback-link-tracker.js
@@ -5,8 +5,8 @@
     init: function() {
       $('#callback-link').on('click', function(evt) {
         evt.preventDefault();
-        if(ga) {
-          ga('send', 'event', 'callback', 'requested', window.location.pathname);
+        if(window.ga) {
+          window.ga('send', 'event', 'callback', 'requested', window.location.pathname);
         }
         window.location.href = this.href;
       });
