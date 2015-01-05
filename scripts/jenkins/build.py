3fab776379b0692af757c8ffb7a6c361f33bcce9
Andy Driver <andy.driver@digital.justice.gov.uk>
2015-01-05 15:43:07 +0000

    Pass test backend server fixtures on start

--- scripts/jenkins/build.py
+++ scripts/jenkins/build.py
@@ -102,6 +102,19 @@ def run_server(
 
     venv = '/tmp/jenkins/envs/{0}-{1}'.format(project_name, env)
 
+    fixtures = (
+        'initial_groups.json '
+        'kb_from_knowledgebase.json '
+        'initial_category.json '
+        'test_provider.json '
+        'initial_mattertype.json '
+        'test_auth_clients.json '
+        'initial_media_codes.json '
+        'test_rotas.json '
+        'test_casearchived.json '
+        'test_providercases.json '
+        'test_provider_allocations.json')
+
     run((
         'cd {workspace} && '
         '{venv}/bin/python manage.py testserver {fixtures} --addrport {port} '
