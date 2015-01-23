5611f2f3799aa4073086f4dbaba9488e018a114c
Josh Rowe <josh@s-block.com>
2015-01-23 12:07:35 +0000

    turn off threshold tests

--- scripts/jenkins/build.py
+++ scripts/jenkins/build.py
@@ -28,7 +28,7 @@ def parse_args():
         help='e.g. integration, production, etc.')
     parser.add_argument(
         '--threshold-tests',
-        action='store_false',
+        action='store_true',
         help='run threshold tests')
 
     args = parser.parse_args()
