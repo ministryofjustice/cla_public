413615e7a906b2f4bdc9e98a3dbbed618e7bbe97
Andy Driver <andy.driver@digital.justice.gov.uk>
2015-01-27 09:58:49 +0000

    Use OperatorHours from cla_common for callmeback available slots

--- cla_public/libs/call_centre_availability.py
+++ cla_public/libs/call_centre_availability.py
@@ -1,29 +1,24 @@
-import datetime
-from itertools import ifilter, islice, takewhile
 import requests
 
 from flask import current_app
 
-from cla_common.call_centre_availability import *
+from cla_common import call_centre_availability
+from cla_common.call_centre_availability import BankHolidays, available, \
+    available_days, current_datetime, time_slots, today_slots
 
 
-def load_bank_holidays():
-    timeout = current_app.config.get('API_CLIENT_TIMEOUT', None)
-    holidays = requests.get(BANK_HOLIDAYS_URL, timeout=timeout).json()['events']
-    return map(get_date, holidays)
+class FlaskCacheBankHolidays(BankHolidays):
+
+    def init_cache(self):
+        self._cache = current_app.cache
+
+    def _load_dates(self):
+        timeout = current_app.config.get('API_CLIENT_TIMEOUT', None)
+        return requests.get(self.url, timeout=timeout).json()['events']
 
 
-def cache_bank_holidays(bank_holidays):
-    one_year = 365 * 24 * 60 * 60
-    current_app.cache.set('bank_holidays', bank_holidays, timeout=one_year)
-
-
-def bank_holidays():
-    bank_holidays = current_app.cache.get('bank_holidays')
-    if not bank_holidays:
-        bank_holidays = load_bank_holidays()
-        cache_bank_holidays(bank_holidays)
-    return bank_holidays
+# monkey patch cla_common to avoid invoking django code
+call_centre_availability.bank_holidays = lambda: FlaskCacheBankHolidays()
 
 
 def time_choice(time):
@@ -33,7 +28,9 @@ def time_choice(time):
 
 
 def suffix(d):
-    return 'th' if 11 <= d <= 13 else {1: 'st', 2: 'nd', 3: 'rd'}.get(d % 10, 'th')
+    if 11 <= d <= 13:
+        return 'th'
+    return {1: 'st', 2: 'nd', 3: 'rd'}.get(d % 10, 'th')
 
 
 def day_choice(day):
