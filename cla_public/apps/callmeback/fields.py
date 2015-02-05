e4805ddf12279b2d1bb53422bfba0de16660e2a2
Josh Rowe <josh@s-block.com>
2015-02-05 17:59:19 +0000

    if no choices then dont set default

--- cla_public/apps/callmeback/fields.py
+++ cla_public/apps/callmeback/fields.py
@@ -71,7 +71,8 @@ class TimeChoiceField(FormattedChoiceField, SelectField):
     def __init__(self, choices_callback=None, validators=None, **kwargs):
         super(TimeChoiceField, self).__init__(validators=validators, **kwargs)
         self.choices = map(time_choice, choices_callback())
-        self.default, display = random.choice(self.choices)
+        if self.choices:
+            self.default, display = random.choice(self.choices)
 
     def process_formdata(self, valuelist):
         if valuelist:
