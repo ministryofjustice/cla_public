ac163cdb40b89f0b04e9cbfaad4c051445ef5f50
Josh Rowe <josh@s-block.com>
2015-04-08 15:39:36 +0100

    skip review page on ineligible outcomes

--- tests/nightwatch/specs/current/ineligible-outcomes.js
+++ tests/nightwatch/specs/current/ineligible-outcomes.js
@@ -25,8 +25,6 @@ module.exports = {
         .setValue('input[name="savings"]', '50000')
         .setValue('input[name="investments"]', '50000')
         .submitForm('form')
-        .waitForElementVisible('.answers-summary', 5000)
-        .submitForm('form')
         .waitForElementVisible('a[href="https://www.gov.uk/find-a-legal-adviser"]', 5000)
         .assert.urlContains('/help-organisations/' + item.category.label.toLowerCase().replace(/ /g, '-'))
 
