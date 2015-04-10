4c4c5aa6685eb38f22097ae074d91412908f4013
Josh Rowe <josh@s-block.com>
2015-04-10 16:25:50 +0100

    use links to traverse

--- cla_public/apps/scope/urls.py
+++ cla_public/apps/scope/urls.py
@@ -2,11 +2,11 @@
 from cla_public.apps.scope import scope
 from cla_public.apps.scope.views import ScopeDiagnosis, ScopeDiagnosisApiProxy
 
-scope.add_url_rule(
-    'diagnosis', view_func=ScopeDiagnosis.as_view('diagnosis'))
+view = ScopeDiagnosis.as_view('diagnosis')
 
-scope.add_url_rule(
-    'diagnosis/<path:choices>', view_func=ScopeDiagnosis.as_view('diagnosis-path'))
+scope.add_url_rule('diagnosis/', view_func=view)
+
+scope.add_url_rule('diagnosis/<path:choices>', view_func=view)
 
 scope.add_url_rule(
     'api', view_func=ScopeDiagnosisApiProxy.as_view('diagnosis-api-proxy'))
