59fbe7d5f7b2a7145e07fb03b4ffcb065df0f880
Josh Rowe <josh@s-block.com>
2015-04-13 15:58:25 +0100

    remove unneeded diagnosis api proxy from scope app

--- cla_public/apps/scope/urls.py
+++ cla_public/apps/scope/urls.py
@@ -1,12 +1,9 @@
 # -*- coding: utf-8 -*-
 from cla_public.apps.scope import scope
-from cla_public.apps.scope.views import ScopeDiagnosis, ScopeDiagnosisApiProxy
+from cla_public.apps.scope.views import ScopeDiagnosis
 
 view = ScopeDiagnosis.as_view('diagnosis')
 
 scope.add_url_rule('diagnosis/', view_func=view)
 
 scope.add_url_rule('diagnosis/<path:choices>', view_func=view)
-
-scope.add_url_rule(
-    'api', view_func=ScopeDiagnosisApiProxy.as_view('diagnosis-api-proxy'))
