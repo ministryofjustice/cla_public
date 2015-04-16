43ea293a2905acee6e2a621e5dcb582a74c8903f
Tyom Semonov <inspiro@gmail.com>
2015-04-16 13:57:05 +0100

    Implement law categories for legal advisers

--- cla_public/libs/laalaa.py
+++ cla_public/libs/laalaa.py
@@ -1,13 +1,34 @@
-import requests
-import json
-from flask import current_app
+# -*- coding: utf-8 -*-
 
+import requests
+from flask import current_app
+from flask.ext.babel import lazy_gettext as _
+
+
+PROVIDER_CATEGORIES = {
+    'aap': _('Actions against the police'),
+    'med': _('Clinical negligence'),
+    'com': _('Community care'),
+    'crm': _('Crime'),
+    'deb': _('Debt'),
+    'mat': _('Family'),
+    'fmed': _('Family mediation'),
+    'hou': _('Housing'),
+    'immas': _('Immigration or asylum'),
+    'mhe': _('Mental health'),
+    'pl': _('Prison law'),
+    'pub': _('Public law'),
+    'wb': _('Welfare benefits')
+}
 
 class LaaLaaError(Exception):
     pass
 
 
 def find(postcode, page=1):
+    def decode_category(code):
+        return PROVIDER_CATEGORIES.get(code.lower())
+
     try:
         response = requests.get(
             '{host}/legal-advisers/?postcode={postcode}&format=json&page={page}'
@@ -18,7 +39,14 @@ def find(postcode, page=1):
             )
         )
         try:
-            return json.loads(response.content)
+            data = response.json()
+
+            for result in data['results']:
+                if result.get('categories'):
+                    result['categories'] = map(decode_category, result['categories'])
+
+            return data
+
         except ValueError:
             raise LaaLaaError
     except (requests.exceptions.ConnectionError,
