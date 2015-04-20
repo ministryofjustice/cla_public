3a9886c53722d0d9a6d1989e21cdd1333b4eaad4
Clive Murray <clive.murray@digital.justice.gov.uk>
2015-04-20 14:15:10 +0100

    add interstitial page for in-scope journey

--- cla_public/apps/scope/urls.py
+++ cla_public/apps/scope/urls.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from cla_public.apps.scope import scope
 from cla_public.apps.scope.views import ScopeDiagnosis
+from flask import render_template
 
 view = ScopeDiagnosis.as_view('diagnosis')
+
+def in_scope():
+    return render_template('scope/in-scope.html')
 
 scope.add_url_rule('diagnosis/', view_func=view)
 
 scope.add_url_rule('diagnosis/<path:choices>', view_func=view)
+
+scope.add_url_rule('in-scope/', view_func=in_scope)
