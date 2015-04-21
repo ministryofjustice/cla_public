88fcecbae8b167ebadda12c8d2ea3792ad78be70
Clive Murray <clive.murray@digital.justice.gov.uk>
2015-04-21 16:52:15 +0100

    TEST: fix scope diagnosis tests

--- tests/nightwatch/specs/current/scope-diagnosis.js
+++ tests/nightwatch/specs/current/scope-diagnosis.js
@@ -6,26 +6,26 @@ var scenarios = [
   {
     name: 'In scope',
     nodes: ['Debt', 'You own your own home', 'Yes'],
-    destination: '/about',
-    continue: true
+    destination: '/scope/in-scope',
+    identifier: 'a.continue'
   },
   {
     name: 'Category out of scope',
     nodes: ['Clinical negligence'],
     destination: '/result/face-to-face',
-    continue: false
+    identifier: 'input[name="postcode"]'
   },
   {
     name: 'Problem detail out of scope',
     nodes: ['Education', 'Admissions', 'Admission to school in the normal admissions round'],
     destination: '/result/face-to-face',
-    continue: false
+    identifier: 'input[name="postcode"]'
   },
   {
     name: 'Contact',
     nodes: ['Family', 'A problem with your ex-partner', 'Disputes over children', 'Domestic abuse', 'Yes'],
     destination: '/contact',
-    continue: false
+    identifier: 'input[name="third_party_handled"]'
   }
 ];
 
@@ -36,8 +36,8 @@ module.exports = {
     scenarios.forEach(function(scenario) {
       common.startPage(client);
       client
-        .scopeDiagnosis(scenario.name, scenario.nodes, scenario.continue)
-        .pause(1000)
+        .scopeDiagnosis(scenario.name, scenario.nodes)
+        .waitForElementVisible(scenario.identifier, 5000)
         .assert.urlContains(scenario.destination,
           'Destination page URL contains ' + scenario.destination)
       ;
