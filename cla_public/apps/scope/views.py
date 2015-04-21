3c58817d1f171dc14cdb8d82f784a784c4b564b7
Josh Rowe <josh@s-block.com>
2015-04-21 16:14:15 +0100

    refactor scope api

--- cla_public/apps/scope/views.py
+++ cla_public/apps/scope/views.py
@@ -1,20 +1,12 @@
 # -*- coding: utf-8 -*-
-import urllib
 from cla_common.constants import DIAGNOSIS_SCOPE
 from django.template.defaultfilters import striptags
-import requests
-from cla_public.apps.checker.api import post_to_eligibility_check_api
-from cla_public.apps.checker.utils import category_option_from_name
 from cla_public.apps.scope import scope
-from cla_public.libs.utils import override_locale
+from cla_public.apps.scope.api import diagnosis_api_client as api
 from cla_public.libs.views import RequiresSession
-from flask import views, render_template, current_app, request, session, \
-    url_for, redirect
+from flask import views, render_template, current_app, url_for, \
+    redirect
 
-
-# keys in session
-REF_KEY = 'diagnosis_ref'
-PREV_KEY = 'diagnosis_previous_choices'
 
 OUTCOME_URLS = {
     DIAGNOSIS_SCOPE.INSCOPE: '/scope/in-scope',
@@ -34,102 +26,11 @@ def add_header(response):
     return response
 
 
-class ScopeApiMixin(object):
-
-    def request_path(self, path=''):
-        if REF_KEY in session:
-            path = 'diagnosis/%s/%s' % (session[REF_KEY], path)
-        else:
-            path = 'diagnosis/%s' % path
-        return '{host}{path}?{params}'.format(
-            host=current_app.config['BACKEND_API']['url'],
-            path=path,
-            params=urllib.urlencode(request.args))
-
-    def request_args(self):
-        return {
-            'timeout': current_app.config.get('API_CLIENT_TIMEOUT', None)
-        }
-
-    def post_to_scope(self, path='', payload={}):
-        request_args = self.request_args()
-        request_args['json'] = payload
-        return requests.post(self.request_path(path), **request_args)
-
-
-class ScopeDiagnosis(RequiresSession, views.MethodView, ScopeApiMixin):
-    response = None
-
-    def create_diagnosis(self):
-        response = self.post_to_scope()
-        session[REF_KEY] = response.json().get('reference')
-
-    def move(self, previous_choices=[], choices_list=[]):
-        """
-        This enables a user to jump to parts of the diagnosis and we send a
-        request to the api for each step. The api only allows the user to
-        move up or down 1 step at a time.
-
-        :param previous_choices - choices on last request (saved in session):
-        :param choices_list - new choices (from url):
-        :return requests Response object:
-        """
-        prev = len(previous_choices)
-        now = len(choices_list)
-        diff = now - prev
-        if prev == now:
-            # reload page - same choices as before
-            return requests.get(self.request_path(), **self.request_args())
-        elif prev > now:
-            direction = 'up'
-            steps = reversed(previous_choices[diff:])
-        else:
-            direction = 'down'
-            steps = choices_list[-diff:]
-
-        for s in steps:
-            payload = {}
-            payload['current_node_id'] = s
-            resp = self.post_to_scope('move_%s/' % direction, payload=payload)
-        return resp
-
-    def do_move(self, choices):
-        choices_list = [c for c in choices.strip('/').split('/') if c]
-        previous_choices = session.get(PREV_KEY, [])
-        session[PREV_KEY] = choices_list
-
-        return self.move(
-                previous_choices,
-                choices_list)
-
-    def get_category(self, response_json):
-        category = response_json['category']
-        if not category:
-            category_name = striptags(response_json['nodes'][0]['label'])
-            with override_locale('en'):
-                category, name, desc = category_option_from_name(category_name)
-        return category
-
-    def save_category(self, category, note=None):
-        session['category'] = category
-        if category == 'violence':
-            category = 'family'
-        session.add_note(
-            u'User selected category:',
-            unicode(session.category_name))
-        if note:
-            session.add_note(
-                u'Public Diagnosis note:',
-                note)
-        post_to_eligibility_check_api(payload={
-            'category': category
-        })
-
+class ScopeDiagnosis(RequiresSession, views.MethodView):
     def get(self, choices='', *args, **kwargs):
-        if not session.get(REF_KEY):
-            self.create_diagnosis()
+        api.create_diagnosis()
 
-        response = self.do_move(choices)
+        response = api.do_move(choices)
 
         try:
             response_json = response.json()
@@ -145,7 +46,9 @@ class ScopeDiagnosis(RequiresSession, views.MethodView, ScopeApiMixin):
             note = None
             if state == DIAGNOSIS_SCOPE.CONTACT and nodes:
                 note = striptags(nodes[-1]['help_text'])
-            self.save_category(self.get_category(response_json), note)
+            api.save_category(
+                api.get_category(response_json),
+                note)
 
             return redirect(OUTCOME_URLS[state])
 
