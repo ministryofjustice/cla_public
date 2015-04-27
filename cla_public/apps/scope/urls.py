6c5fa10781293d2ea39349f6e96fe42787def0c9
Clive Murray <clive.murray@digital.justice.gov.uk>
2015-04-27 15:03:55 +0100

    bump common to 0.1.72, split help orgs into partial, add ineligible template

--- cla_public/apps/scope/urls.py
+++ cla_public/apps/scope/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from cla_public.apps.scope import scope
-from cla_public.apps.scope.views import ScopeDiagnosis, ScopeInScope
+from cla_public.apps.scope.views import ScopeDiagnosis, ScopeInScope, ScopeIneligible
 from flask import render_template
 
 view = ScopeDiagnosis.as_view('diagnosis')
@@ -10,3 +10,5 @@ scope.add_url_rule('diagnosis/', view_func=view)
 scope.add_url_rule('diagnosis/<path:choices>', view_func=view)
 
 scope.add_url_rule('in-scope/', view_func=ScopeInScope.as_view('in-scope'))
+
+scope.add_url_rule('ineligible/', view_func=ScopeIneligible.as_view('ineligible'))
