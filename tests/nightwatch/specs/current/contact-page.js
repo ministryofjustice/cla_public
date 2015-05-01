ae236a4f3ded3750a792139d1597a9d0a3cf756e
Tyom Semonov <inspiro@gmail.com>
2015-05-01 16:18:19 +0100

    Implement textarea character counter
    Usage:
    Add `data-character-count='4000'` as attribute on textarea.
    
    The value has to be a number. A small template for counter is also required (see contact.html)
    
    [#92355174]

--- tests/nightwatch/specs/current/contact-page.js
+++ tests/nightwatch/specs/current/contact-page.js
@@ -44,6 +44,33 @@ module.exports = {
 
     willCallWithNotes(client, text(4000));
     assertConfirmation(client);
+  },
+
+  'Notes have counter': function(client) {
+    contactPage(client);
+
+    client
+      .setValue('textarea[name="extra_notes"]', text(2000), function() {
+        console.log('     • Adding 2000 characters in notes field');
+      })
+      .assert.containsText('.character-counter', 2000,
+        'Checking that notes counter shows 2000 left')
+      .setValue('textarea[name="extra_notes"]', text(1980), function() {
+        console.log('     • Adding another 1980 characters');
+      })
+      .assert.containsText('.character-counter', 20,
+        'Checking that notes counter shows 20 left')
+      .assert.cssClassPresent('.character-counter', 'counter-low',
+        'Counter class should change to ‘low character’ mode')
+      .setValue('textarea[name="extra_notes"]', text(30), function() {
+        console.log('     • Overflowing notes field with 10 characters over the limit');
+      })
+      .assert.containsText('.character-counter', 0,
+        'Checking that notes counter shows 0')
+      .getValue('textarea[name="extra_notes"]', function(response) {
+        this.assert.ok(response.value.length === 4000,
+          'Ensure that number of characters in notes field is still 4000');
+      });
 
     client.end();
   }
