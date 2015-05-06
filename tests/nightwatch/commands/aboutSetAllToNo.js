ed86b6200302dd4156ec0ebc1775c4677e922180
Tyom Semonov <inspiro@gmail.com>
2015-05-06 14:05:50 +0100

    TEST: Add options to add exceptions for aboutSetAllToNo()

--- tests/nightwatch/commands/aboutSetAllToNo.js
+++ tests/nightwatch/commands/aboutSetAllToNo.js
@@ -1,10 +1,13 @@
 'use strict';
 
+var _ = require('lodash');
 var ABOUT_YOU_QUESTIONS = require('../modules/constants').ABOUT_YOU_QUESTIONS;
 var log = require('../modules/log');
+var common = require('../modules/common-functions');
 
-exports.command = function(shouldSubmitForm, callback) {
+exports.command = function(shouldSubmitForm, options, callback) {
   var client = this;
+  options = options || {};
 
   this.perform(function() {
     log.command('Processing About you page…');
@@ -13,8 +16,17 @@ exports.command = function(shouldSubmitForm, callback) {
       .setYesNoFields(ABOUT_YOU_QUESTIONS, 0, function() {
         console.log('     • All values set to ‘No’');
       })
-      .conditionalFormSubmit(shouldSubmitForm)
     ;
+
+    if(options instanceof Object) {
+      _.each(options, function(value, name) {
+        client.setYesNoFields(name, value, function() {
+          console.log('       • Additionally, `' + name + '` set to ‘' + common.humaniseValue(value) + '’');
+        });
+      });
+    }
+
+    client.conditionalFormSubmit(shouldSubmitForm);
   });
 
   if (typeof callback === 'function') {
