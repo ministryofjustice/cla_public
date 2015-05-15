19511d122b3003a203c91e50a38d5b901617f6d1
Tyom Semonov <inspiro@gmail.com>
2015-05-15 15:42:36 +0100

    TEST: Refactor `fillInIncome` and `fillInOutgoings`
    To allow custom values

--- tests/nightwatch/modules/common-functions.js
+++ tests/nightwatch/modules/common-functions.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var util = require('util');
+var _ = require('lodash');
 var SAVINGS_QUESTIONS = require('../modules/constants').SAVINGS_QUESTIONS;
 SAVINGS_QUESTIONS.ALL = SAVINGS_QUESTIONS.MONEY.concat(SAVINGS_QUESTIONS.VALUABLES);
 
@@ -77,5 +78,42 @@ module.exports = {
     };
 
     return yesNo[value] || value;
+  },
+
+  formatMoneyInputs: function(prefix, inputs) {
+    var result = {};
+    _.each(inputs, function(v, k) {
+      if(_.isObject(v)) {
+        _.map(v, function(value, period) {
+          result[util.format('%s%s-per_interval_value', prefix, k)] = value;
+          result[util.format('%s%s-interval_period', prefix, k)] = period;
+        });
+      } else {
+        result[util.format('%s%s', prefix, k)] = v;
+      }
+    });
+    return result;
+  },
+
+  fillInMoneyForm: function(client, inputs, type) {
+    _.each(inputs, function(v, k) {
+      var selector = util.format('[name="%s"]', k);
+      client.elements('css selector', selector, function(result) {
+        if(!result.value.length) {
+          return;
+        }
+
+        if(typeof v === 'number') {
+          client.setValue(selector, v, function() {
+            console.log('     • %s: %s is £%d', type, k, v);
+          });
+        } else {
+          selector += util.format(' [value="%s"]', v);
+          client.click(selector, function() {
+            console.log('       • %s selected', v);
+          });
+        }
+      });
+    });
   }
 };
