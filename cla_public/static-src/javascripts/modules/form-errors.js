48a3d6a1df8d3b53ca0184b4417615d7343c9586
Josh Rowe <josh@s-block.com>
2015-05-26 15:43:25 +0100

    refactor form-errors.js and add scroll after errors have been added

--- cla_public/static-src/javascripts/modules/form-errors.js
+++ cla_public/static-src/javascripts/modules/form-errors.js
@@ -7,33 +7,47 @@
     },
 
     bindEvents: function() {
-      $('form').on('submit',  $.proxy(this.postToFormErrors, this));
+      $('button[type="submit"]', $('form')).on('click',  $.proxy(this.postToFormErrors, this));
     },
 
     postToFormErrors: function(e) {
-      var $form = $(e.currentTarget);
-      var self = this;
-      $.ajax({
-        type: 'OPTIONS',
-        url: '',
-        contentType: "application/x-www-form-urlencoded",
-        data: $form.serialize(),
-        async: false,
-        success: function(errors) {
-          if (!$.isEmptyObject(errors)) {
-            e.preventDefault();
-            e.stopPropagation();
-            self.loadErrors(errors);
-          }
-        }
-      });
+      this.$form = $(e.currentTarget).closest('form');
+      if (this.$form.length) {
+        e.preventDefault();
+        e.stopPropagation();
+        $.ajax({
+          type: 'OPTIONS',
+          url: '',
+          contentType: 'application/x-www-form-urlencoded',
+          data: this.$form.serialize()
+        }).done(
+          $.proxy(this.onAjaxSuccess, this)
+        ).fail(
+          $.proxy(this.onAjaxError, this)
+        );
+      }
+    },
+
+    onAjaxSuccess: function (errors) {
+      if (!$.isEmptyObject(errors)) {
+        this.loadErrors(errors);
+        $('html, body').animate({
+            scrollTop: $('.m-error:visible:first').offset().top - 50
+        }, 300);
+      } else {
+        this.$form.submit();
+      }
+    },
+
+    onAjaxError: function () {
+      this.$form.submit();
     },
 
     loadErrors: function (errors) {
       var errorFields = {};
 
       (function fieldName (errorsObj, prefix) {
-        prefix = (typeof prefix == 'undefined')? '': prefix + '-';
+        prefix = (typeof prefix === 'undefined')? '': prefix + '-';
         for (var key in errorsObj) {
           var field = prefix + key;
           if ($.isArray(errorsObj[key])) {
