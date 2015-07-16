0ceaf62ce7768d30a14c053972c9272bd05c2930
Tyom Semonov <inspiro@gmail.com>
2015-07-16 13:26:40 +0100

    NPM: Add Browser Sync HTML Injector
    HTML Injector attempts to watch template files edits and reload
    only corresponding part of edited HTML instead of the whole page.

--- tasks/serve.js
+++ tasks/serve.js
@@ -2,7 +2,8 @@
 
 var gulp = require('gulp');
 var paths = require('./_paths');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
+var htmlInjector = require("bs-html-injector");
 var argv = require('yargs').argv;
 
 
@@ -11,13 +12,17 @@ gulp.task('serve', ['build'], function() {
   var host = argv.host || argv.h || 'localhost';
   var port = argv.port || argv.p || 5000;
 
-  browserSync({
+  browserSync.use(htmlInjector, {
+    files: '*.html'
+  });
+
+  browserSync.init({
     proxy: host + ':' + port,
     open: false,
     port: 3000
   });
 
-  gulp.watch(paths.templates, browserSync.reload);
+  gulp.watch(paths.templates, htmlInjector);
   gulp.watch(paths.images, ['images']);
   gulp.watch(paths.ie_styles, ['ie-css']);
   gulp.watch(paths.styles, ['sass']);
