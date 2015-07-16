a99dd2c1fb7477ff93c2b7e96f0332e2b0b564d0
Tyom Semonov <inspiro@gmail.com>
2015-07-16 11:03:17 +0100

    BE: Remove duplicate honeypot file

--- cla_public/apps/checker/views.py
+++ cla_public/apps/checker/views.py
@@ -20,12 +20,11 @@ from cla_public.apps.checker.forms import AboutYouForm, YourBenefitsForm, \
     ReviewForm, AdditionalBenefitsForm
 from cla_public.apps.checker.means_test import MeansTest, MeansTestError
 from cla_public.apps.checker.validators import IgnoreIf
-from cla_public.apps.checker import honeypot
 from cla_public.apps.checker import filters  # Used in templates
 from cla_public.libs.utils import override_locale, category_id_to_name
 from cla_public.libs.views import AllowSessionOverride, FormWizard, \
     FormWizardStep, RequiresSession, ValidFormOnOptions, HasFormMixin
-from cla_public.libs import laalaa
+from cla_public.libs import laalaa, honeypot
 
 
 log = logging.getLogger(__name__)
