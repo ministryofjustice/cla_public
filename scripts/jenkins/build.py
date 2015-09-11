15a360e58a83b86309e7eac933aca6307a38ca67
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-09-11 12:12:55 +0100

    updating jenkins build script for new jenkins environment - removed threshold test option as these integration tests no longer exist - now running backend in a separate, cleaned folder rather than in it's jenkins workspace

--- scripts/jenkins/build.py
+++ scripts/jenkins/build.py
@@ -12,7 +12,7 @@ from Queue import Queue
 logging.basicConfig(level='INFO')
 log = logging.getLogger(__name__)
 
-PROJECT_NAME = "cla_public"
+PROJECT_NAME = 'cla_public'
 
 background_processes = Queue()
 
@@ -20,21 +20,17 @@ background_processes = Queue()
 def parse_args():
     parser = argparse.ArgumentParser(
         description='Build project ready for testing by Jenkins.')
-    parser.add_argument(
-        'envname',
-        metavar='envname',
-        type=str,
-        nargs=1,
-        help='e.g. integration, production, etc.')
-    parser.add_argument(
-        '--threshold-tests',
-        action='store_true',
-        help='run threshold tests')
+    parser.add_argument('envname', type=str,
+                        help='e.g. integration, production, etc.')
+    parser.add_argument('--backend-hash', type=str, default='develop',
+                        help='cla_backend commit to run tests against; '
+                             'defaults to latest develop branch commit')
 
     args = parser.parse_args()
     return {
-        'envname': args.envname[0],
-        'threshold_tests': args.threshold_tests}
+        'envname': args.envname,
+        'backend_hash': args.backend_hash,
+    }
 
 
 def run(command, background=False, **kwargs):
@@ -59,12 +55,14 @@ def make_virtualenv(env):
         env=env)
 
     if not os.path.isdir(venv_path):
-        run('virtualenv --no-site-packages {path}'.format(path=venv_path))
+        run('/usr/local/bin/virtualenv {path}'.format(path=venv_path))
 
     return venv_path
 
 
 def install_dependencies(venv_path):
+    run('{venv}/bin/pip install -U setuptools pip wheel'.format(
+        venv=venv_path))
     run('{venv}/bin/pip install -r requirements/jenkins.txt'.format(
         venv=venv_path))
     run('npm install')
@@ -83,7 +81,7 @@ def wait_until_available(url):
     wget.wait()
 
 
-def remove_old_template_js():
+def remove_old_static_assets():
     run('rm -f cla_public/static-src/javascripts/templates.js')
 
 
@@ -91,58 +89,80 @@ def update_static_assets():
     run('gulp')
 
 
-def _port(start_from=8000, up_to=8999):
+def compile_messages(venv_path):
+    """Compile message.po files to mo files for the translations"""
+    run('{venv}/bin/pybabel compile -d cla_public/translations'.format(venv=venv_path))
+
+
+def _port(start_from=8300, up_to=8499):
     port = random.randint(start_from, up_to)
     while True:
         yield port
         port += 1
 
 
-def run_server(
-        env,
-        project_name='cla_backend',
-        port_env_var='CLA_BACKEND_PORT',
-        project_dir=None):
+gen_port = _port()
 
-    if not project_dir:
-        if env == 'production':
-            project_dir = '/srv/jenkins/workspace/CLA Backend - production'
-        else:
-            project_dir = '/srv/jenkins/workspace/CLA Backend - integration'
 
-    port = next(_port())
-    os.environ[port_env_var] = '{0}'.format(port)
+def run_server(env, backend_hash=''):
+    venv = '/tmp/jenkins/envs/cla_backend-%s' % env
+    project_dir = '/srv/jenkins/shared-backend/%s-%s' % (PROJECT_NAME, env)
+    if not os.path.isdir(project_dir):
+        os.makedirs(project_dir)
 
-    venv = '/tmp/jenkins/envs/{0}-{1}'.format(project_name, env)
+    if not os.path.isdir(os.path.join(project_dir, '.git')):
+        run('cd {project_dir} && git clone https://github.com/ministryofjustice/cla_backend.git .'.format(
+            project_dir=project_dir,
+        ))
+    if backend_hash:
+        run('cd {project_dir} && git fetch --prune && git checkout -f {backend_hash}'.format(
+            project_dir=project_dir,
+            backend_hash=backend_hash,
+        ))
+    else:
+        run('cd {project_dir} && git fetch --prune && git checkout develop && git pull'.format(
+            project_dir=project_dir,
+        ))
+
+    backend_port = next(gen_port)
+    os.environ['CLA_BACKEND_PORT'] = str(backend_port)
+    os.environ['BACKEND_TEST_DB_SUFFIX'] = '4%s' % PROJECT_NAME
 
     fixtures = (
-        'initial_groups.json '
-        'kb_from_knowledgebase.json '
-        'initial_category.json '
-        'test_provider.json '
-        'initial_mattertype.json '
-        'test_auth_clients.json '
-        'initial_media_codes.json '
-        'test_rotas.json '
-        'test_casearchived.json '
-        'test_providercases.json '
-        'test_provider_allocations.json')
+        'initial_groups.json',
+        'kb_from_knowledgebase.json',
+        'initial_category.json',
+        'test_provider.json',
+        'initial_mattertype.json',
+        'test_auth_clients.json',
+        'initial_media_codes.json',
+        'test_rotas.json',
+        'test_casearchived.json',
+        'test_providercases.json',
+        'test_provider_allocations.json',
+        # TODO: add complaints category fixtures once that branch is merged
+    )
 
-    run((
-        'cd {workspace} && '
-        '{venv}/bin/python manage.py testserver {fixtures} --addrport {port} '
-        '--noinput --settings=cla_backend.settings.jenkins > /dev/null').format(
-            workspace=project_dir.replace(' ', '\ '),
+    run(('cd {project_dir} && '
+         '{venv}/bin/python manage.py testserver {fixtures} '
+         '--addrport {port} --noinput '
+         '--settings=cla_backend.settings.jenkins '
+         '> {project_dir}/testserver.log~').format(
+            project_dir=project_dir,
             venv=venv,
-            fixtures=fixtures,
-            port=port),
+            fixtures=' '.join(fixtures),
+            port=backend_port),
         background=True)
 
 
-def run_tests(venv_path, threshold_tests=False):
+def run_tests(venv_path):
+    wait_until_available('http://localhost:{port}/admin/'.format(
+        port=os.environ.get('CLA_BACKEND_PORT'))
+    )
+
     config = 'CLA_PUBLIC_CONFIG=config/jenkins.py'
-    port = next(_port())
-    os.environ['CLA_PUBLIC_PORT'] = '{0}'.format(port)
+    public_port = next(gen_port)
+    os.environ['CLA_PUBLIC_PORT'] = str(public_port)
     run('{conf} {venv}/bin/nosetests --with-xunit'.format(
         venv=venv_path,
         conf=config))
@@ -150,13 +170,10 @@ def run_tests(venv_path, threshold_tests=False):
         '{conf} {venv}/bin/python manage.py mockserver -p {port} -D -R'.format(
             venv=venv_path,
             conf=config,
-            port=port),
+            port=public_port),
         background=True)
-    wait_until_available('http://localhost:{port}/'.format(port=port))
-    skipgroup = ' -s legacy'
-    if threshold_tests:
-        skipgroup = ''
-    run('./nightwatch -c tests/nightwatch/jenkins.json{0} -M'.format(skipgroup))
+    wait_until_available('http://localhost:{port}/'.format(port=public_port))
+    run('./nightwatch -c tests/nightwatch/jenkins.json -M')
 
     # nightwatch fails to clean up these process
     # NB: if two jobs are running on the same jenkins slave then one may break the other
@@ -169,32 +186,9 @@ def kill_child_processes(pid, sig=signal.SIGTERM):
         shell=True,
         stdout=subprocess.PIPE)
     ps_out = ps_cmd.stdout.read()
-    retcode = ps_cmd.wait()
+    ps_cmd.wait()
     for pid_str in ps_out.split('\n')[:-1]:
         os.kill(int(pid_str), sig)
-
-
-def compile_messages(venv_path):
-    """Compile message.po files to mo files for the translations"""
-    run('{venv}/bin/pybabel compile -d cla_public/translations'.format(venv=venv_path))
-
-
-def main():
-    try:
-        args = parse_args()
-        env = args['envname']
-        venv_path = make_virtualenv(env)
-        install_dependencies(venv_path)
-        remove_old_template_js()
-        update_static_assets()
-        compile_messages(venv_path)
-        clean_pyc()
-        run_server(env)
-        wait_until_available('http://localhost:{port}/admin/'.format(
-            port=os.environ.get('CLA_BACKEND_PORT')))
-        run_tests(venv_path, args['threshold_tests'])
-    finally:
-        kill_all_background_processes()
 
 
 def kill_all_background_processes():
@@ -205,6 +199,23 @@ def kill_all_background_processes():
             process.kill()
         except OSError:
             pass
+
+
+def main():
+    try:
+        args = parse_args()
+        env = args['envname']
+        backend_hash = args['backend_hash']
+        venv_path = make_virtualenv(env)
+        install_dependencies(venv_path)
+        remove_old_static_assets()
+        update_static_assets()
+        compile_messages(venv_path)
+        clean_pyc()
+        run_server(env, backend_hash)
+        run_tests(venv_path)
+    finally:
+        kill_all_background_processes()
 
 
 if __name__ == '__main__':
