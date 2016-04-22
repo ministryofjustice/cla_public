ead1b6e85f40945a061da704021b2ef0a6dafcc3
Josh Rowe <josh@s-block.com>
2016-04-22 15:14:11 +0100

    custom validation of emails  - limit chars in emails as was causing error when spaces allowed in email name  - relates to /mojds/prod-public/group/541/

---
+++ cla_public/apps/contact/validators.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import re
+
+from wtforms.validators import Email, HostnameValidation
+
+
+class EmailValidator(Email):
+    def __init__(self, message=None):
+        self.validate_hostname = HostnameValidation(
+            require_tld=True,
+        )
+        super(Email, self).__init__(
+            r'^[-0-9a-zA-Z.+_\'*+-/=?^_`{|}~]+@([^.@][^@]+)$',
+            re.IGNORECASE,
+            message)
