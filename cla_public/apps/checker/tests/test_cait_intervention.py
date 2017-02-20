209516a450e785c6c783ef09c740a347da5f8861
Alex Robinson <github@alex.fu2k.org>
2017-02-20 15:05:35 +0000

    Fix useless CAIT tests

--- cla_public/apps/checker/tests/test_cait_intervention.py
+++ cla_public/apps/checker/tests/test_cait_intervention.py
@@ -17,53 +17,53 @@ for state in ['on', 'off']:
 
 
 # Provide a mock response for the tests
-def setup_mock(json, status):
+def setup_mock(mock_json, mock_status):
     def mocked_requests_get(*args, **kwargs):
         class MockResponse:
-            def __init__(self, json_data, status_code):
-                self.json_data = json
-                self.status_code = status
+            def __init__(self, mock_json, mock_status):
+                self.json_data = mock_json
+                self.status_code = mock_status
 
             def json(self):
                 return self.json_data
-        return MockResponse(json, status)
+        return MockResponse(mock_json, mock_status)
     return mocked_requests_get
 
 # Call the cait intervention code with standard params
 def call_cait_params():
     return get_cait_params({'foo': 'bar', 'truncate': 5}, 'Family', [], {})
 # Params unaffected by cait intervention
-default_params_out = {'foo': 'bar', 'truncate': 5}
-
+DEFAULT_PARAMS_OUT = {'foo': 'bar', 'truncate': 5}
+PARAMS_OUT_WITH_SURVEY = {'foo': 'bar', 'truncate': 5, 'cait_survey':{}}
+# Path to requests.get
+REQUESTS_GET = 'cla_public.apps.checker.cait_intervention.requests.get'
 
 class TestCaitIntervention(unittest.TestCase):
 
     # Simulate a config file not being returned
-    @mock.patch('cla_public.apps.checker.cait_intervention.requests.get', side_effect=setup_mock(None, 404))
+    @mock.patch(REQUESTS_GET, side_effect=setup_mock(None, 404))
     def test_config_missing(self, mock_get):
-        params = call_cait_params()
-        self.assertEqual(params, default_params_out)
+        with app.test_request_context('/scope/refer/family'):
+            params = call_cait_params()
+            self.assertEqual(params, DEFAULT_PARAMS_OUT)
 
     # Simulate a config file being returned that is invalid
-    @mock.patch('cla_public.apps.checker.cait_intervention.requests.get', side_effect=setup_mock(None, 200))
-    def test_config_missing(self, mock_get):
-        params = call_cait_params()
-        self.assertEqual(params, default_params_out)
+    @mock.patch(REQUESTS_GET, side_effect=setup_mock(None, 200))
+    def test_config_invalid(self, mock_get):
+        with app.test_request_context('/scope/refer/family'):
+            params = call_cait_params()
+            self.assertEqual(params, DEFAULT_PARAMS_OUT)
 
     # Simulate a config file being returned that has off values
-    @mock.patch('cla_public.apps.checker.cait_intervention.requests.get', side_effect=setup_mock(configs['off'], 200))
-    def test_config_missing(self, mock_get):
-        params = call_cait_params()
-        self.assertEqual(params, default_params_out)
-    
-    # Simulate a config file being returned that has on values
-    @mock.patch('cla_public.apps.checker.cait_intervention.requests.get', side_effect=setup_mock(configs['on'], 200))
-    def test_config_on(self, mock_get):
-  
-        # Shouldn't run if user has gone through financial check
-        with app.test_request_context('/scope/checker/family'):
+    @mock.patch(REQUESTS_GET, side_effect=setup_mock(configs['off'], 200))
+    def test_config_off(self, mock_get):
+        with app.test_request_context('/scope/refer/family'):
             params = call_cait_params()
-            self.assertEqual(params, default_params_out)
+            self.assertEqual(params, PARAMS_OUT_WITH_SURVEY)
+
+    # Simulate a config file being returned that has on values
+    @mock.patch(REQUESTS_GET, side_effect=setup_mock(configs['on'], 200))
+    def test_config_on(self, mock_get):
 
         # Finally, check updated params
         with app.test_request_context('/scope/refer/family'):
@@ -83,3 +83,8 @@ class TestCaitIntervention(unittest.TestCase):
             params = call_cait_params()
             self.assertEqual(params.get('cait_variant'), 'default')
             self.assertEqual(params.get('truncate'), 5)
+
+        # Shouldn't run if user has gone through financial check
+        with app.test_request_context('/scope/checker/family'):
+            params = call_cait_params()
+            self.assertEqual(params, DEFAULT_PARAMS_OUT)
