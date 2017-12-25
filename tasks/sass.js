61ad50e71be4dde9deeb0dfe8b0552b37b29cc2a
Alistair Laing <aliuk2012@users.noreply.github.com>
2017-12-25 21:43:29 +0000

    updates gulp tasks in line with updated packages

--- tasks/sass.js
+++ tasks/sass.js
@@ -8,14 +8,13 @@ var browserSync = require('browser-sync');
 var reload = browserSync.reload;
 
 gulp.task('sass', ['clean-css'], function() {
-  return gulp
-    .src(paths.src + 'stylesheets/**/*.scss')
-    .pipe(sass({
+  return sass(paths.src + 'stylesheets/**/*.scss', { 
+      sourcemap: true,
       lineNumbers: true,
       style: 'compact',
       loadPath: 'node_modules/govuk_frontend_toolkit/' // add node module toolkit path
-    }))
-    .on('error', function (err) { console.log(err.message); })
+    })
+    .on('error', sass.logError )
     .pipe(gulp.dest(paths.dest + 'stylesheets/'))
     .pipe(filter('**/*.css'))  // don't react to .map files when reloading browser
     .pipe(reload({ stream:true }));
