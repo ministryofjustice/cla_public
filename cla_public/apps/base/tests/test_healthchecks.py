73ee55368fd597d016f170f69d49b997d8ad0c00
David Lantos <david.lantos@digital.justice.gov.uk>
2018-08-21 13:36:18 +0100

    Add backend API healthcheck test

--- cla_public/apps/base/tests/test_healthchecks.py
+++ cla_public/apps/base/tests/test_healthchecks.py
@@ -1,6 +1,8 @@
 import unittest
 import mock
+import requests
 
+from cla_public import app
 from cla_public.apps.base import healthchecks
 
 
@@ -34,3 +36,35 @@ class DiskSpaceHealthcheckTest(unittest.TestCase):
         result = healthchecks.check_disk()
         self.assertDictContainsSubset(
             {'status': False, 'available_percent': 2.0}, result)
+
+
+class BackendAPIHealthcheckTest(unittest.TestCase):
+    def setUp(self):
+        self.app = app.create_app('config/testing.py')
+        self.app.test_request_context().push()
+
+    @mock.patch('requests.get')
+    def test_backend_check_fails_if_request_fails(self, request_mock):
+        request_mock.side_effect = requests.exceptions.ConnectionError()
+
+        result = healthchecks.check_backend_api()
+        self.assertDictContainsSubset(
+            {'status': False, 'response': 'ConnectionError'}, result)
+
+    @mock.patch('requests.get')
+    def test_backend_check_fails_if_backend_is_unhealthy(self, request_mock):
+        request_mock.return_value.ok = False
+        request_mock.return_value.json.return_value = {'status': 'DOWN'}
+
+        result = healthchecks.check_backend_api()
+        self.assertDictContainsSubset(
+            {'status': False, 'response': {'status': 'DOWN'}}, result)
+
+    @mock.patch('requests.get')
+    def test_backend_check_passes_if_backend_is_healthy(self, request_mock):
+        request_mock.return_value.ok = True
+        request_mock.return_value.json.return_value = {'status': 'UP'}
+
+        result = healthchecks.check_backend_api()
+        self.assertDictContainsSubset(
+            {'status': True, 'response': {'status': 'UP'}}, result)
