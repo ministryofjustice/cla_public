cae842a22270a13d7bf459da3dce9f0641a648cf
Nick Rowlands <nick@rowlando.com>
2018-09-14 20:04:25 +0100

    set locale with query param

--- cla_public/apps/base/__init__.py
+++ cla_public/apps/base/__init__.py
@@ -1,5 +1,16 @@
 "Base app"
-from flask import Blueprint
-
+import datetime
+from flask import Blueprint, after_this_request, request
 
 base = Blueprint('base', __name__)
+
+@base.before_app_request
+def detect_user_locale():
+    locale = request.args.get('locale')
+    
+    if locale is not None:
+        @after_this_request
+        def remember_locale(response):
+            expires = datetime.datetime.now() + datetime.timedelta(days=30)
+            response.set_cookie('locale', locale, expires=expires)
+            return response
