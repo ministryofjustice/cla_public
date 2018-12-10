d1f3b989345190b07b6902d5628bcffc6401b36e
Patrick Farrell <patrick.farrell@digital.justice.gov.uk>
2018-12-10 14:39:09 +0000

    Clean up old view code and unnecessary setting

--- cla_public/apps/geocoder/views.py
+++ cla_public/apps/geocoder/views.py
@@ -8,7 +8,6 @@ from cla_common.address_lookup.ordnance_survey import OsAddressLookupFormatted
 from flask import Response, current_app
 
 from cla_public.apps.geocoder import geocoder
-from cla_public.apps.geocoder.mock_geocoder import mock_geocoder
 
 log = logging.getLogger(__name__)
 
@@ -16,10 +15,6 @@ log = logging.getLogger(__name__)
 @geocoder.route('/addresses/<postcode>', methods=['GET'])
 def geocode(postcode):
     """Lookup addresses with the specified postcode"""
-
-    if current_app.config.get('TESTING'):
-        return mock_geocoder(postcode)
-
     key = current_app.config.get('OS_PLACES_API_KEY')
     formatted_addresses = OsAddressLookupFormatted(key=key).lookup_postcode(postcode)
     response = [{'formatted_address': address} for address in formatted_addresses if address]
