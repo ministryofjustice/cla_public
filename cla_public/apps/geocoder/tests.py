7fe0d96f241af1fb14b53c1b3a7651987aabce21
Patrick Farrell <patrick.farrell@digital.justice.gov.uk>
2018-12-12 10:38:16 +0000

    Restore dummy API key to test

--- cla_public/apps/geocoder/tests.py
+++ cla_public/apps/geocoder/tests.py
@@ -1,12 +1,18 @@
 import json
-import mock
 import unittest
 
+import mock
+from cla_public.app import create_app
 from cla_public.apps.geocoder.views import geocode
 
 
 class GeocoderTest(unittest.TestCase):
     prerecorded_result = "Ministry of Justice\n52 Queen Annes Gate\nLondon\nSW1H 9AG"
+
+    def setUp(self):
+        app = create_app("config/testing.py")
+        app.config["OS_PLACES_API_KEY"] = "DUMMY_TOKEN"
+        app.test_request_context().push()
 
     def test_response_packaging(self):
         expected_formatted_result = json.dumps([{"formatted_address": self.prerecorded_result}])
