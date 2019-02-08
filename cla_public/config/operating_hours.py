70b3d6327762bfe39d0ab236405975c064b7b940
said-moj <said.shire@digital.justice.gov.uk>
2019-02-08 14:43:49 +0000

    Stop application from booting up without essential configuration

---
+++ cla_public/config/operating_hours.py
@@ -0,0 +1,7 @@
+import datetime
+OPERATOR_HOURS = {
+    "weekday": (datetime.time(9, 0), datetime.time(20, 0)),
+    "saturday": (datetime.time(9, 0), datetime.time(12, 30)),
+    "2018-12-24": (datetime.time(9, 0), datetime.time(17, 30)),
+    "2018-12-31": (datetime.time(9, 0), datetime.time(17, 30)),
+}
