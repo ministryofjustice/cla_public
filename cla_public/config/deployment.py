b97f037224162a12752ac991ba07b3ee10d31766
said-moj <said.shire@digital.justice.gov.uk>
2019-03-28 10:19:46 +0000

    These variables are defined in config/common.py: BACKEND_BASE_URI, LAALAA_API_HOST, SECRET_KEY

--- cla_public/config/deployment.py
+++ cla_public/config/deployment.py
@@ -8,6 +8,8 @@ settings_required = (
     "SMTP_PASSWORD",
     "RAVEN_CONFIG_DSN",
     "RAVEN_CONFIG_SITE",
+    "BACKEND_BASE_URI",
+    "LAALAA_API_HOST",
 )
 
 for key in settings_required:
@@ -19,15 +21,9 @@ from cla_public.config.common import *
 
 DEBUG = os.environ.get("SET_DEBUG", False) == "True"
 
-SECRET_KEY = os.environ["SECRET_KEY"]
-
 SESSION_COOKIE_SECURE = os.environ.get("CLA_ENV", "") in ["prod", "staging"]
 
 HOST_NAME = os.environ.get("HOST_NAME") or os.environ.get("HOSTNAME")
-
-BACKEND_BASE_URI = os.environ["BACKEND_BASE_URI"]
-
-LAALAA_API_HOST = os.environ.get("LAALAA_API_HOST", "https://prod.laalaa.dsd.io")
 
 LOGGING["handlers"]["console"]["formatter"] = "logstash"
 LOGGING["loggers"] = {"": {"handlers": ["console"], "level": os.environ.get("LOG_LEVEL", "INFO")}}
