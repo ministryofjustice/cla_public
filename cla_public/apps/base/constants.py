eddaaec4f52d05481820fa6c9393f167573debf9
joshlow <19589707+joshlow@users.noreply.github.com>
2019-08-21 11:10:35 +0100

    Form label changes
    Changes to form labels

--- cla_public/apps/base/constants.py
+++ cla_public/apps/base/constants.py
@@ -16,10 +16,9 @@ FEEL_ABOUT_SERVICE = zip(FEEL_ABOUT_SERVICE_LABELS, FEEL_ABOUT_SERVICE_LABELS)
 # Feedback: help filling in the form
 HELP_FILLING_IN_FORM_LABELS = [
     _(u"No, I filled in this form myself"),
-    _(u"I have difficulty using computers so someone filled in this form for me"),
+    _(u"I had help filling in this form"),
+    _(u"Someone else filled in this form for me"),
     _(u"I used an accessibility tool such as a screen reader"),
-    _(u"I had some other kind of help"),
-]
 
 HELP_FILLING_IN_FORM = zip(HELP_FILLING_IN_FORM_LABELS, HELP_FILLING_IN_FORM_LABELS)
 
