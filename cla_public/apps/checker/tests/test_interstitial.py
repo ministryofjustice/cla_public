3c071b24ffff4765c77db86858461025bf2cbd72
said-moj <45761276+said-moj@users.noreply.github.com>
2019-09-11 10:32:52 +0100

    Adding violence,education and debt and discrimination to list of categories to show laalaa link

---
+++ cla_public/apps/checker/tests/test_interstitial.py
@@ -0,0 +1,10 @@
+import unittest
+
+from cla_public.apps.checker.views import get_show_laalaa_link_categories
+
+
+class TestInterstitial(unittest.TestCase):
+    def test_should_show_laalaa_categories(self):
+        self.assertEqual(
+            ["debt", "discrimination", "family", "housing", "violence"], get_show_laalaa_link_categories()
+        )
