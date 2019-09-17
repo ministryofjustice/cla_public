eb89ba84bf7d6381c4a37039ad8a5dfe50bb6954
said-moj <said.shire@digital.justice.gov.uk>
2019-09-17 17:10:49 +0100

    Override the default cookie consent to allow only essentials by default

--- cla_public/static-src/javascripts/modules/cookie-banner.js
+++ cla_public/static-src/javascripts/modules/cookie-banner.js
@@ -1,7 +1,17 @@
  'use strict';
-  require('govuk_publishing_components/components/cookie-banner.js');
   require('govuk_publishing_components/lib/cookie-functions.js');
+  require('govuk_publishing_components/components/cookie-banner.js');
 
+
+  window.GOVUK.setDefaultConsentCookie = function () {
+    var defaultConsent = {
+      'essential': true,
+      'settings': true,
+      'usage': false,
+      'campaigns': false
+    }
+    window.GOVUK.setCookie('cookie_policy', JSON.stringify(defaultConsent), { days: 365 })
+  }
 
 
 
