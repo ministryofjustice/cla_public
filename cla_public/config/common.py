2371a158047d31191a8e9808959f4c093100499f
said-moj <45761276+said-moj@users.noreply.github.com>
2019-09-25 12:37:20 +0100

    Move the GDS GA ID to an environment variable
    Also renamed GA_ID environment variable to MOJ_GA_ID as the application now has
    multiple GA ids

--- cla_public/config/common.py
+++ cla_public/config/common.py
@@ -69,7 +69,8 @@ ZENDESK_API_USERNAME = os.environ.get("ZENDESK_API_USERNAME")
 ZENDESK_API_TOKEN = os.environ.get("ZENDESK_API_TOKEN")
 ZENDESK_DEFAULT_REQUESTER = 649762516  # anonymous feedback <noreply@ministryofjustice.zendesk.com>
 
-GA_ID = os.environ.get("GA_ID")
+MOJ_GA_ID = os.environ.get("MOJ_GA_ID")
+GDS_GA_ID = os.environ.get("GDS_GA_ID")
 
 CACHE_TYPE = "simple"
 
