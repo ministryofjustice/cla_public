6485a13830b88004f08bed2cc5eca52d78484c13
Malcolm Butler <32877315+MoJ-Longbeard@users.noreply.github.com>
2020-01-10 11:24:42 +0000

    LGA-822 Radio Buttons Styling

--- cla_public/apps/contact/constants.py
+++ cla_public/apps/contact/constants.py
@@ -6,4 +6,4 @@ from flask.ext.babel import lazy_gettext as _
 
 DAY_TODAY = "today"
 DAY_SPECIFIC = "specific_day"
-DAY_CHOICES = ((DAY_TODAY, _("Call today at")), (DAY_SPECIFIC, _("Call on")))
+DAY_CHOICES = ((DAY_TODAY, _("Call today")), (DAY_SPECIFIC, _("Call on another day")))
