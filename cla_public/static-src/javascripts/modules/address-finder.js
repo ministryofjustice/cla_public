85a736d381dba667d051d22b2847a7ad9aad43ee
Malcolm Butler <32877315+MoJ-Longbeard@users.noreply.github.com>
2020-01-23 13:15:04 +0000

    LGA-831 Postcode Lookup Styling and Accessibility

--- cla_public/static-src/javascripts/modules/address-finder.js
+++ cla_public/static-src/javascripts/modules/address-finder.js
@@ -118,25 +118,87 @@
       } else {
         this.$postcodeRow.after(this.listTemplate({items: addrItems, count: addresses.length}));
       }
-      $('.address-list .form-control').focus();
+      $('#address-finder').focus();
+
+      var place;
+      var link;
+      var irishPostcode = "BT";
+      var manxPostcode = "IM";
+      var jerseyPostcode = "JE";
+      var guernseyPostcode = "GY";
+      var scottishPostcode = [
+          "AB",
+          "DD",
+          "DG",
+          "EH",
+          "FK",
+          "G1",
+          "G2",
+          "G3",
+          "G4",
+          "G5",
+          "G6",
+          "G7",
+          "G8",
+          "G9",
+          "G0",
+          "HS",
+          "IV",
+          "KA",
+          "KW",
+          "KY",
+          "ML",
+          "PA",
+          "PH",
+          "TD",
+          "ZE",
+        ];
+
+      if ($("#address-post_code").val().substr(0,2) == irishPostcode) {
+        var country = "Northern Ireland";
+        var anchor = '<a class="govuk-link" href="https://www.nidirect.gov.uk/articles/legal-aid-schemes">nidirect.gov.uk</a>';
+      }
+      else if ($("#address-post_code").val().substr(0,2) == manxPostcode) {
+        var country = "the Isle of Man";
+        var anchor = '<a class="govuk-link" href="https://www.gov.im/categories/benefits-and-financial-support/legal-aid/">gov.im</a>';
+      }
+      else if ($("#address-post_code").val().substr(0,2) == jerseyPostcode) {
+        var country = "Jersey";
+        var anchor = '<a class="govuk-link" href="https://www.legalaid.je/">legalaid.je</a>';
+      }
+      else if ($("#address-post_code").val().substr(0,2) == guernseyPostcode) {
+        var country = "Guernsey";
+        var anchor = '<a class="govuk-link" href="https://www.gov.gg/legalaid">gov.gg</a>';
+      }
+      else if (scottishPostcode.indexOf($("#address-post_code").val().substr(0,2)) !== -1) {
+        var country = "Scotland";
+        var anchor = '<a class="govuk-link" href="https://www.mygov.scot/legal-aid/">mygov.scot</a>';
+      } else {
+        var england = true;
+      }
+      if (!england) $(".address-list").after(this.geoTemplate({place: country, link: anchor}));
+
+
     },
 
     showError: function (template) {
       this.$formGroup
-        .addClass('m-error');
+        .addClass('govuk-form-group--error');
       this.$formGroup.find('.form-row')
         .before(template);
     },
 
     reset: function () {
       this.$formGroup
-        .removeClass('m-error')
-        .find('.field-error').remove();
+        .removeClass('govuk-form-group--error')
+        .find('.govuk-error-message').remove();
 
       $('.address-list').remove();
+      $('.geographyWarning').remove();
     },
 
     templates: function () {
+      this.geoTemplate = _.template($('#geoWarning').html());
       this.listTemplate = _.template($('#addressFinderList').html());
       this.buttonTemplate = _.template($('#addressFinderButton').html());
       this.postcodeNotEnteredTemplate = _.template($('#postcodeNotEnteredText').html());
