3092e790db3bf991d9d67675f7ed2f411bfa08a9
Malcolm Butler <32877315+MalcolmVonMoJ@users.noreply.github.com>
2020-04-15 11:59:28 +0100

    Update cla_public/apps/base/extensions.py
    Co-Authored-By: Michael Blatherwick <michael.blatherwick@digital.justice.gov.uk>

--- cla_public/apps/base/extensions.py
+++ cla_public/apps/base/extensions.py
@@ -19,5 +19,4 @@ def is_quick_exit_enabled(nodes, session=None):
         quick_exit_categories = ["n43n3", "n18", "n19", "n88", "n86", "n97", "n149"]
         for category in quick_exit_categories:
             return category in session.checker["diagnosis_previous_choices"]
-                return category
     return False
