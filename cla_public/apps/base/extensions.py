5ba87b1b72a06631aa4f4c31fc3d174ee0151eb0
Michael Blatherwick <michael.blatherwick@exeter.oxon.org>
2020-04-17 12:16:29 +0100

    Fix quick exit function

--- cla_public/apps/base/extensions.py
+++ cla_public/apps/base/extensions.py
@@ -18,5 +18,6 @@ def is_quick_exit_enabled(session):
     if "diagnosis_previous_choices" in session.checker:
         quick_exit_categories = ["n43n3", "n18", "n19", "n88", "n86", "n97", "n149"]
         for category in quick_exit_categories:
-            return category in session.checker["diagnosis_previous_choices"]
+            if category in session.checker["diagnosis_previous_choices"]:
+                return True
     return False
