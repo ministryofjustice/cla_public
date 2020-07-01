9d936411951024e88c2d74bd2048ffda93921fb8
Mayowa <mayowa.adeniyi@digital.justice.gov.uk>
2020-07-01 12:43:53 +0100

    Add more tests for CheckerTaggedJSONSerializer

--- cla_public/apps/checker/tests/test_sessions.py
+++ cla_public/apps/checker/tests/test_sessions.py
@@ -4,9 +4,6 @@ from flask import json
 from base64 import b64encode
 from flask._compat import text_type
 from werkzeug.http import http_date
-
-
-
 from datetime import datetime
 from cla_public.apps.checker.session import CheckerTaggedJSONSerializer, CheckerSessionObject, MeansTest, CheckerSessionInterface
 
@@ -18,11 +15,18 @@ class TestCheckerSession(unittest.TestCase):
     def assert_json(self, value):
         try:
             json.loads(value)
-        except JSONDecodeError:
+        except Exception:
             self.fail('Not valid JSON')
 
     def format_json(self, value):
         return json.dumps(value, separators=(",", ":"))
+
+    def compare_dicts(self, outputDict, expectedDict):
+        for key, value in expectedDict.items():
+            if isinstance(value, dict):
+                self.compare_dicts(outputDict[key], value)
+            else:
+                self.assertEqual(outputDict[key], value)
 
     def test_serializer_with_checker_session_object(self):
         value = CheckerSessionObject({'checker': {'foo': 'bar'}})
@@ -32,18 +36,10 @@ class TestCheckerSession(unittest.TestCase):
         self.assert_json(jsonOutput)
         self.assertEqual(jsonOutput, expectedJSON)
 
-    def compare_dicts(self, outputDict, expectedDict):
-        for key, value in expectedDict.items():
-            if isinstance(value, dict):
-                self.compare_dicts(outputDict[key], value)
-            else:
-                self.assertEqual(outputDict[key], value)
-
-    def test_means_test_input(self):
+    def test_serializer_with_means_test(self):
         value = MeansTest()
         outputJSON = self.serializer.dumps(value)
         expectedDict = {u" mt": {u"on_passported_benefits": {u" b": b64encode("0").decode("ascii")}, u"specific_benefits": {}, u"dependants_old": 0, u"you": {u"savings": {u"credit_balance": 0, u"investment_balance": 0, u"asset_balance": 0, u"bank_balance": 0}, u"deductions": {u"income_tax": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"mortgage": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"childcare": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"rent": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"maintenance": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"criminal_legalaid_contributions": 0, u"national_insurance": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}}, u"income": {u"self_employment_drawings": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"benefits": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"maintenance_received": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"self_employed": {u" b": b64encode("0").decode("ascii")}, u"tax_credits": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"earnings": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"child_benefits": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"other_income": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}, u"pension": {u"per_interval_value": 0, u"interval_period": {u" b": b64encode("per_month").decode("ascii")}}}}, u"dependants_young": 0, u"on_nass_benefits": {u" b": b64encode("0").decode("ascii")}}}
-
         expectedJSON = self.format_json(expectedDict)
         self.assert_json(outputJSON)
         outputDict = json.loads(outputJSON)
@@ -108,13 +104,27 @@ class TestCheckerSession(unittest.TestCase):
         expectedJSON = self.format_json(expectedDict)
         self.assert_json(jsonOutput)
         self.assertEqual(jsonOutput, expectedJSON)
-#
-#     def test_str_input(self):
-#         value = "hello"
-#         serialize = self.serializer.dumps(value)
-#         self.assertIsInstance(serialize, str)
-#
-#     def test_int_input(self):
-#         value = 5
-#         serialize = self.serializer.dumps(value)
-#         self.assertIsInstance(serialize, str)
+
+    def test_serializer_with_str(self):
+        value = "hello"
+        jsonOutput = self.serializer.dumps(value)
+        expectedDict = {" b": b64encode(value).decode("ascii")}
+        expectedJSON = self.format_json(expectedDict)
+        self.assert_json(jsonOutput)
+        self.assertEqual(jsonOutput, expectedJSON)
+
+    def test_serializer_with_int(self):
+        value = 5
+        jsonOutput = self.serializer.dumps(value)
+        expectedValue = 5
+        expectedJSON = self.format_json(expectedValue)
+        self.assert_json(jsonOutput)
+        self.assertEqual(jsonOutput, expectedJSON)
+
+    def test_serializer_with_unicode_str(self):
+        value = u"hello"
+        jsonOutput = self.serializer.dumps(value)
+        expectedValue = u"hello"
+        expectedJSON = self.format_json(expectedValue)
+        self.assert_json(jsonOutput)
+        self.assertEqual(jsonOutput, expectedJSON)
