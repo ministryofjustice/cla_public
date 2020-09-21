152fe6f2733030a699dd28ef13cfd55ca3e78ccc
Mayowa <mayowa.adeniyi@digital.justice.gov.uk>
2020-09-21 11:50:07 +0100

    Correct tests

--- cla_public/apps/checker/tests/test_money_field.py
+++ cla_public/apps/checker/tests/test_money_field.py
@@ -112,15 +112,15 @@ class TestMoneyField(unittest.TestCase):
 
     def test_pound_sign_as_prefix(self):
         self.assertValidAmount("£100")
-        self.assertAmount("10000", "£100")
+        self.assertAmount(10000, "£100")
         self.assertValidAmount("£435.34")
-        self.assertAmount("43534", "£435.34")
+        self.assertAmount(43534, "£435.34")
         self.assertValidAmount("£5000.2")
-        self.assertAmount("5000020", "£5000.2")
+        self.assertAmount(500020, "£5000.2")
         self.assertValidAmount("£3,412.21")
-        self.assertAmount("341221", "£3,412.21")
+        self.assertAmount(341221, "£3,412.21")
         self.assertValidAmount("£3,90   ")
-        self.assertAmount("39000", "£3,90   ")
+        self.assertAmount(39000, "£3,90   ")
 
     def test_pound_sign_with_negative_number(self):
         self.assertValidAmount("£-5000.2")
