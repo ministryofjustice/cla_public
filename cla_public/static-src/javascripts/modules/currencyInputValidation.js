ff1e1917461dafc7eeaaf06a24af0735cb84d4bf
Mayowa <mayowa.adeniyi@digital.justice.gov.uk>
2020-09-29 19:37:30 +0100

    Add functionality for validating currency input
    This will match the validation that is on the back end

--- cla_public/static-src/javascripts/modules/currencyInputValidation.js
+++ cla_public/static-src/javascripts/modules/currencyInputValidation.js
@@ -1,7 +1,36 @@
 // ($(this).val() && Number($(this).val()) > 0)
 
 function isValidAmount(input) {
-    return input && Number(input) > 0
+    var maxVal = 100000000
+    var minVal = 0
+
+    var value = input.split('.')
+    var pounds = value[0]
+    var pence = value[1]
+    pounds = pounds.replaceAll(/^£|[\s,]+/g, "")
+
+    if(pence) {
+        var lengthOfPence = pence.length
+        if(lengthOfPence > 2) {
+            return false
+        }
+    }
+
+    var amount = Number(pounds * 100)
+    if(pence) {
+        amount += Number(pence)
+    }
+
+    if(amount) {
+        console.log('Amount: ' + amount)
+        if(amount > maxVal || amount < minVal) {
+            return false
+        }
+    } else {
+        return false
+    }
+
+    return true
 }
 
 exports.isValidAmount = isValidAmount
