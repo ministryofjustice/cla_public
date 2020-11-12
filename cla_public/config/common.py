1a3ce6fd71d9bf944dbf37378fbc3a18dd22a3c6
said-moj <said.shire@digital.justice.gov.uk>
2020-11-12 13:16:34 +0000

    Remove MOJ GA ID from code as this done through GTM now

--- cla_public/config/common.py
+++ cla_public/config/common.py
@@ -67,7 +67,6 @@ ZENDESK_API_USERNAME = os.environ.get("ZENDESK_API_USERNAME")
 ZENDESK_API_TOKEN = os.environ.get("ZENDESK_API_TOKEN")
 ZENDESK_DEFAULT_REQUESTER = 649762516  # anonymous feedback <noreply@ministryofjustice.zendesk.com>
 
-MOJ_GA_ID = os.environ.get("MOJ_GA_ID")
 GDS_GA_ID = os.environ.get("GDS_GA_ID")
 MOJ_GTM_ID = os.environ.get("MOJ_GTM_ID")
 MOJ_GTM_AUTH = os.environ.get("MOJ_GTM_AUTH")
