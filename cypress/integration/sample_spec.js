62fd212fae97c4fd054e83462f4d4405b0401856
Mayowa <mayowa.adeniyi@digital.justice.gov.uk>
2020-12-17 14:13:04 +0000

    Answer no for each question on 'About You' form page and submit
    The answering of each question will be later refactored into a helper function, so we have more control over what and how we answer

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -11,5 +11,13 @@ describe('Mini test', function() {
     cy.contains('Legal aid is available for this type of problem')
     cy.contains('Check if you qualify financially').click()
 
+    cy.get(".govuk-radios__item input[value='0']").each(($el, index, $list) => {
+      if($el.css("visibility") !== "hidden") {
+        cy.wrap($el).check();
+      }
+    })
+
+    cy.get("#submit-button").click();
+
   });
 })
