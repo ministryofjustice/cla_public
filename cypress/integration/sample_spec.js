87508f55c0fa8ea0f66e1f5e66c9f9b87b4ae642
Mayowa <mayowa.adeniyi@digital.justice.gov.uk>
2021-01-07 12:27:19 +0000

    Fill in more form choices

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -11,8 +11,18 @@ describe('Mini test', function() {
     cy.contains('Legal aid is available for this type of problem')
     cy.contains('Check if you qualify financially').click()
 
-    cy.setRadioInput('have_partner')
-    cy.setRadioInput('on_benefits', 'No')
+    // About you page form
+    cy.setRadioInput('have_partner', 'Yes')
+    cy.setRadioInput('in_dispute', 'No')  // Depends on have_partner being set to 'Yes'
+    cy.setRadioInput('on_benefits', 'Yes')
+    cy.setRadioInput('have_children')
+    cy.setRadioInput('have_dependants')
+    cy.setRadioInput('own_property')
+    cy.setRadioInput('is_employed')
+    cy.setRadioInput('is_self_employed')
+    cy.setRadioInput('aged_60_or_over')
+    cy.setRadioInput('have_savings')
+    cy.setRadioInput('have_valuables')
 
     // cy.get("#submit-button").click();
 
