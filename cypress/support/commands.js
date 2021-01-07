70a197cf1bac524b1d57cd7eb4a3f955e8772d81
Mayowa <mayowa.adeniyi@digital.justice.gov.uk>
2021-01-07 12:28:25 +0000

    Refactor

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -12,7 +12,7 @@
 // -- This is a parent command --
 // Cypress.Commands.add("login", (email, password) => { ... })
 //
-//
+//mm
 // -- This is a child command --
 // Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
 //
@@ -25,10 +25,6 @@
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('setRadioInput', (selector, value) => {
-  const selectorValue = value === 'Yes' ? '1' : '0'
-  cy.get(`input[name="${selector}"]`).each(($el, index, $list) => {
-    if($el.val() === selectorValue) {
-      cy.wrap($el).check();
-    }
-  })
+  const selectorValue = value === 'Yes' ? '0' : '1'
+  cy.get(`#${selector}-${selectorValue}`).check();
 })
