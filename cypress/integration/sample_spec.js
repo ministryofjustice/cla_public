2bbd65af8b38ee754a3f1e5c66ba48cebb1a9789
Mayowa <mayowa.adeniyi@digital.justice.gov.uk>
2021-01-11 20:07:11 +0000

    Complete happy path journey and refactor

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -15,17 +15,17 @@ describe('Mini test', function() {
 
     // About you form
     cy.contains('About you')
-    cy.setRadioInput('have_partner', 'No')
-    cy.setRadioInput('on_benefits', 'Yes')
-    cy.setRadioInput('have_children', 'Yes')
+    cy.setYesNoRadioInput('have_partner', 'No')
+    cy.setYesNoRadioInput('on_benefits', 'Yes')
+    cy.setYesNoRadioInput('have_children', 'Yes')
     cy.setTextInput('num_children', '4')
-    cy.setRadioInput('have_dependants', 'No')
-    cy.setRadioInput('own_property', 'Yes')
-    cy.setRadioInput('is_employed', 'No')
-    cy.setRadioInput('is_self_employed', 'No')
-    cy.setRadioInput('aged_60_or_over', 'No')
-    cy.setRadioInput('have_savings', 'Yes')
-    cy.setRadioInput('have_valuables', 'No')
+    cy.setYesNoRadioInput('have_dependants', 'No')
+    cy.setYesNoRadioInput('own_property', 'Yes')
+    cy.setYesNoRadioInput('is_employed', 'No')
+    cy.setYesNoRadioInput('is_self_employed', 'No')
+    cy.setYesNoRadioInput('aged_60_or_over', 'No')
+    cy.setYesNoRadioInput('have_savings', 'Yes')
+    cy.setYesNoRadioInput('have_valuables', 'No')
     cy.get("#submit-button").click();
 
     // Which benefits do you receive form
@@ -38,20 +38,20 @@ describe('Mini test', function() {
     cy.contains('Your additional benefits')
     cy.setCheckboxInput('benefits-6')
     cy.setCheckboxInput('benefits-10')
-    cy.setRadioInput('other_benefits', 'No')
+    cy.setYesNoRadioInput('other_benefits', 'No')
     cy.get("#submit-button").click();
 
     // Your property form
     cy.contains('Your property')
-    cy.setRadioInput('properties-0-is_main_home', 'Yes')
-    cy.setRadioInput('properties-0-other_shareholders', 'Yes')
+    cy.setYesNoRadioInput('properties-0-is_main_home', 'Yes')
+    cy.setYesNoRadioInput('properties-0-other_shareholders', 'Yes')
     cy.setTextInput('properties-0-property_value', randomValue)
     cy.setTextInput('properties-0-mortgage_remaining', randomValue)
     cy.setTextInput('properties-0-mortgage_payments', randomValue)
-    cy.setRadioInput('properties-0-is_rented', 'Yes')
+    cy.setYesNoRadioInput('properties-0-is_rented', 'Yes')
     cy.setTextInput('properties-0-rent_amount-per_interval_value', randomValue)
     cy.setSelectInput('properties-0-rent_amount-interval_period', 'per_week')
-    cy.setRadioInput('properties-0-in_dispute', 'Yes')
+    cy.setYesNoRadioInput('properties-0-in_dispute', 'Yes')
     cy.get("#submit-button").click();
 
     // Your savings form
@@ -69,8 +69,12 @@ describe('Mini test', function() {
     // Contact Civil Legal Advice page
     cy.contains('Contact Civil Legal Advice')
     cy.setTextInput('full_name', 'Michael')
-    cy.setRadioInput('contact_type')
+    cy.setRadioInput('contact_type-1')
     cy.setTextInput('callback-contact_number', '11111111111')
+    cy.setRadioInput('callback-time-specific_day-1')
+    cy.setSelectInput('callback-time-day', 'Tuesday 12th')
+    cy.setSelectInput('callback-time-time_in_day', '2:00 PM - 2:30 PM')
+    cy.get("#submit-button").click();
 
   });
 })
