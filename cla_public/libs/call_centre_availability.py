0e892ae6a20c04b31cc88779fc3b98b74acc575d
said-moj <said.shire@digital.justice.gov.uk>
2021-01-13 15:54:42 +0000

    Translate AM/PM 12-hour clock prefixes

--- cla_public/libs/call_centre_availability.py
+++ cla_public/libs/call_centre_availability.py
@@ -3,10 +3,37 @@ from cla_public.libs.utils import get_locale
 from flask.ext.babel import lazy_gettext as _
 
 
+AFTERNOON = datetime.time(hour=12, minute=0)
+EVENING_NIGHT = datetime.time(hour=18, minute=0)
+
+
+def format_time_welsh_suffix(time):
+    if time >= EVENING_NIGHT:
+        return "yh"
+    elif time >= AFTERNOON:
+        return "yp"
+    else:
+        return "yb"
+
+
+def format_time(dt):
+    if isinstance(dt, datetime.datetime):
+        time = dt.time()
+    else:
+        time = dt
+    display_format = "%I:%M"
+    display_string = time.strftime(display_format).lstrip("0")
+    if get_locale()[:2] == "cy":
+        time_suffix = format_time_welsh_suffix(time)
+    else:
+        time_suffix = time.strftime("%p")
+
+    return "%s %s" % (display_string, time_suffix)
+
+
 def time_choice(time):
-    display_format = "%I:%M %p"
     end = time + datetime.timedelta(minutes=30)
-    display_string = time.strftime(display_format).lstrip("0") + " - " + end.strftime(display_format).lstrip("0")
+    display_string = format_time(time) + " - " + format_time(end)
     return time.strftime("%H%M"), display_string
 
 
--- cla_public/libs/tests/test_utils.py
+++ cla_public/libs/tests/test_utils.py
@@ -1,6 +1,8 @@
+import datetime
 import unittest
 
 from cla_public.libs.utils import category_id_to_name
+from cla_public.libs.call_centre_availability import format_time_welsh_suffix
 
 
 class UtilsTest(unittest.TestCase):
@@ -8,3 +10,24 @@ class UtilsTest(unittest.TestCase):
         category_id = "clinneg"
         result = category_id_to_name(category_id)
         self.assertEquals(u"Clinical negligence", result)
+
+    def test_format_time_welsh_suffix__morning(self):
+        time = datetime.time(hour=0, minute=0)
+        self.assertEqual("yb", format_time_welsh_suffix(time))
+
+        time = datetime.time(hour=11, minute=59)
+        self.assertEqual("yb", format_time_welsh_suffix(time))
+
+    def test_format_time_welsh_suffix__afternoon(self):
+        time = datetime.time(hour=12, minute=0)
+        self.assertEqual("yp", format_time_welsh_suffix(time))
+
+        time = datetime.time(hour=17, minute=59)
+        self.assertEqual("yp", format_time_welsh_suffix(time))
+
+    def test_format_time_welsh_suffix__evening_and_night(self):
+        time = datetime.time(hour=18, minute=0)
+        self.assertEqual("yh", format_time_welsh_suffix(time))
+
+        time = datetime.time(hour=23, minute=59)
+        self.assertEqual("yh", format_time_welsh_suffix(time))
