b3c74b7cd0c117895eef2b6ed649d22f207fdfff
Mayowa <mayowa.adeniyi@digital.justice.gov.uk>
2021-01-18 13:48:18 +0000

    Refactor to enable cypress to work locally or on circle

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,29 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add("login", (email, password) => { ... })
-//
-//mm
-// -- This is a child command --
-// Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add("dismiss", { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
-
 require('cypress-downloadfile/lib/downloadFileCommand')
 
 Cypress.Commands.add('setYesNoRadioInput', (selector, value) => {
@@ -48,6 +22,6 @@ Cypress.Commands.add('setRadioInput', selector => {
 
 Cypress.Commands.add('savePage', fileName => {
   cy.url().then(url => {
-    cy.downloadFile(url, '/home/circleci/project/cypress/pages', `${fileName}.html`)
+    cy.downloadFile(url, 'cypress/pages' , `${fileName}.html`)
   })
 })
