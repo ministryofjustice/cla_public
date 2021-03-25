87d0b219442eeaed734badb577fa64247aed92eb
said-moj <said.shire@digital.justice.gov.uk>
2021-03-25 11:47:50 +0000

    Check if SMART_SURVEY_CID is set before include js for survey

--- cla_public/static-src/javascripts/modules/smart-sruvey.js
+++ cla_public/static-src/javascripts/modules/smart-sruvey.js
@@ -2,6 +2,9 @@
 
   moj.Modules.SmartSurvey = {
     init: function() {
+      if (window.SMART_SURVEY_CID == "") {
+        return;
+      }
       window._ssq={'cid':window.SMART_SURVEY_CID};
       var ss=document.createElement('script');
       ss.type='text/javascript';
