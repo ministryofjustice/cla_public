3b7b5f2fa6fccfd970e0cf4e38ab18aaa4e666ec
psweeting <patrick.sweeting@digital.justice.gov.uk>
2022-12-14 12:57:11 +0000

    LGA-2302 updating SELECT_OPTION_DEFAULT to allow Welsh translation

--- cla_public/apps/contact/constants.py
+++ cla_public/apps/contact/constants.py
@@ -7,7 +7,7 @@ from flask.ext.babel import lazy_gettext as _
 DAY_TODAY = "today"
 DAY_SPECIFIC = "specific_day"
 DAY_CHOICES = ((DAY_TODAY, _("Call today")), (DAY_SPECIFIC, _("Call on another day")))
-SELECT_OPTION_DEFAULT = [("", "-- Please select --")]
+SELECT_OPTION_DEFAULT = [("", _("-- Please select --"))]
 TIME_TODAY_VALIDATION_ERROR = u"Select what time you want to be called today"
 DAY_SPECIFIC_VALIDATION_ERROR = u"Select which day you want to be called"
 TIME_SPECIFIC_VALIDATION_ERROR = u"Select what time you want to be called"
