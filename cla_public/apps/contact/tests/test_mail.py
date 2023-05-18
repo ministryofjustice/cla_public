18017d6337ab01ff9bc3a0e63274e3c66b6642de
Jo Gilder <3441835+jogilder@users.noreply.github.com>
2023-05-18 11:31:10 +0100

    LGA-2451 added new test for confirmation page

--- cla_public/apps/contact/tests/test_mail.py
+++ cla_public/apps/contact/tests/test_mail.py
@@ -87,3 +87,14 @@ class TestConfirmationEmail(unittest.TestCase):
         session.stored["callback_requested"] = True
         create_and_send_confirmation_email(govuk_notify, form.data)
         self.assert_email_arguments(govuk_notify, template_id=GOVUK_NOTIFY_TEMPLATES["PUBLIC_CALLBACK_THIRD_PARTY"])
+
+    def test_confirmation_no_callback(self):
+        govuk_notify = MagicMock()
+        form = submit_and_store_in_session()
+        dictform = form.data
+        dictform.pop("full_name")
+        session.stored["callback_requested"] = False
+        create_and_send_confirmation_email(govuk_notify, dictform)
+        self.assert_email_arguments(
+            govuk_notify, template_id=GOVUK_NOTIFY_TEMPLATES["PUBLIC_CONFIRMATION_NO_CALLBACK"]
+        )
