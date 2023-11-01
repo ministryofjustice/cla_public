28553cc6df5e99fe9342339a6293ea5126613e9a
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-11-01 14:53:37 +0000

    Removed django TestCase

--- cla_public/apps/base/tests/test_notify_feature_flag.py
+++ cla_public/apps/base/tests/test_notify_feature_flag.py
@@ -1,9 +1,8 @@
 from cla_public.apps.base.govuk_notify.api import GovUkNotify, NotifyEmailOrchestrator
-from django.test import TestCase
 from mock import patch
 
 
-class TestNotifyFeatureFlagEnabled(TestCase):
+class TestNotifyFeatureFlagEnabled():
     @patch('cla_public.config.config.USE_EMAIL_ORCHESTRATOR_FLAG', True)
     def test_feature_flag_enabled(self):
         client = GovUkNotify()
@@ -11,7 +10,7 @@ class TestNotifyFeatureFlagEnabled(TestCase):
         assert not isinstance(client, GovUkNotify)
 
 
-class TestNotifyFeatureFlagDisabled(TestCase):
+class TestNotifyFeatureFlagDisabled():
     @patch('cla_public.config.config.USE_EMAIL_ORCHESTRATOR_FLAG', False)
     def test_feature_flag_disabled(self):
         client = GovUkNotify()
