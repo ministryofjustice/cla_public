d55f3b74bb1667ef401822c8ccd05475e73b33ca
Ben Millar <ben.millar@digital.justice.gov.uk>
2023-11-14 16:16:47 +0000

    Added docstring

--- cla_public/apps/base/govuk_notify/api.py
+++ cla_public/apps/base/govuk_notify/api.py
@@ -23,12 +23,26 @@ class NotifyEmailOrchestrator(object):
         return base_url + self.endpoint
 
     def send_email(self, email_address, template_id, personalisation=None):
+        """
+        Sends an email to the Email Orchestration API.
+
+            Parameters:
+                email_address (str) - Email address of the reciever
+                template_id (str) - The GOV.UK Notify template id
+                personalisation (optional, dictionary) - The personalisation dictionary
+
+            Returns:
+                send_api_request (bool) - Was the request sent, will return True if the request was made succesfully
+                                          will return False if the EMAIL_ORCHESTATOR_URL is not set or
+                                          the application is in TESTING or DEBUG mode
+        """
         if TESTING or DEBUG:
-            log.info("Application is in TESTING mode, will not send email")
-            return
+            log.info("Application is in TESTING mode, will not send the request")
+            return False
 
         if not self.base_url:
             log.error("EMAIL_ORCHESTRATOR_URL is not set, unable to send email")
+            return False
 
         data = {"email_address": email_address, "template_id": template_id}
         if personalisation:
@@ -38,3 +52,4 @@ class NotifyEmailOrchestrator(object):
 
         if response.status_code != 201:
             raise HTTPError(response)
+        return True
