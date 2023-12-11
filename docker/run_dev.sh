a418087e4b56e28a8e819e63186c75e3be0fb89d
psweeting <patrick.sweeting@digital.justice.gov.uk>
2023-12-11 14:41:17 +0000

    WIP: Revert Dockerfile back to master

--- docker/run_dev.sh
+++ docker/run_dev.sh
@@ -4,5 +4,4 @@ set -e
 echo "I was called"
 
 # used to generate static files for local development.
-
 BACKEND_BASE_URI=http://localhost:8010 CLA_PUBLIC_CONFIG=config/local.py ./manage.py runserver
