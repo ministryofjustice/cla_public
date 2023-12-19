4080b33106dd3cc904873522d5209de16704b686
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2023-12-19 16:12:29 +0000

    Add in common files

--- cla_public/apps/contact/views.py
+++ cla_public/apps/contact/views.py
@@ -31,6 +31,7 @@ def add_no_cache_headers(response):
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0"
     response.headers["Pragma"] = "no-cache"
     response.headers["X-Content-Type-Options"] = "nosniff"
+    response.headers["Content-Security-Policy"] = "default-src 'self'"
     return response
 
 
