95e0c8beb4992a4f0d74c68f4718b514ad88e683
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2023-12-19 10:44:36 +0000

    Add in additional linting

--- cla_public/apps/checker/views.py
+++ cla_public/apps/checker/views.py
@@ -47,7 +47,7 @@ def add_header(response):
     """
     response.headers["Cache-Control"] = "no-cache, no-store, max-age=0"
     response.headers["Pragma"] = "no-cache"
-    response.headers['X-Content-Type-Options'] = 'nosniff'
+    response.headers["X-Content-Type-Options"] = "nosniff"
     return response
 
 
