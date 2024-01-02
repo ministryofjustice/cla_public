85999d7274753838986791e417c02806a841549f
Kyle O'Brien <65071578+TawneeOwl@users.noreply.github.com>
2024-01-02 15:12:00 +0000

    Add in ajax to script

--- cla_public/app.py
+++ cla_public/app.py
@@ -56,6 +56,7 @@ def create_app(config_file=None):
             "*.ggpht.com",
             "*.googleusercontent.com",
             "blob:",
+            "ajax.aspnetcdn.com",
             "*.googletagmanager.com",
             "*.analytics.google.com",
             "*.g.doubleclick.net",
