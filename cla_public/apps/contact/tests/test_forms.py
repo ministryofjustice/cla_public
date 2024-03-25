5b8774bd74baf9236a4eab156a160fdb97f0a91a
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-03-25 17:58:02 +0000

    Mock ContactForm

--- cla_public/apps/contact/tests/test_forms.py
+++ cla_public/apps/contact/tests/test_forms.py
@@ -1,9 +1,7 @@
 from unittest import TestCase
 from flask import current_app
-from mock import patch, MagicMock
-from cla_public.apps.contact.forms import ContactForm
+from mock import patch
 from cla_public.apps.checker.constants import CONTACT_PREFERENCE, CONTACT_PREFERENCE_NO_CALLBACK
-import datetime
 
 
 class TestContactFormChoices(TestCase):
@@ -11,30 +9,27 @@ class TestContactFormChoices(TestCase):
         pass
 
     @patch("cla_public.apps.contact.forms.ContactForm.update_contact_preference")
-    @patch("cla_public.apps.contact.api.get_valid_callback_days", MagicMock())
-    def test_feature_flag_disabled(self, update_preference):
+    @patch("cla_public.apps.contact.forms.ContactForm")
+    def test_feature_flag_disabled(self, update_preference, ContactForm):
         current_app.config["USE_BACKEND_CALLBACK_SLOTS"] = False
         ContactForm()
         update_preference.assert_not_called()
 
     @patch("cla_public.apps.contact.forms.ContactForm.update_contact_preference")
-    @patch("cla_public.apps.contact.api.get_valid_callback_days", MagicMock())
-    def test_feature_flag_enabled(self, update_preference):
+    @patch("cla_public.apps.contact.forms.ContactForm")
+    def test_feature_flag_enabled(self, update_preference, ContactForm):
         current_app.config["USE_BACKEND_CALLBACK_SLOTS"] = True
         ContactForm()
         update_preference.assert_called()
 
-    @patch("cla_public.apps.contact.api.get_valid_callback_days", MagicMock(return_value=[]))
-    def test_no_slots_available(self):
+    @patch("cla_public.apps.contact.forms.ContactForm")
+    def test_no_slots_available(self, ContactForm):
         current_app.config["USE_BACKEND_CALLBACK_SLOTS"] = True
         form = ContactForm()
         form.contact_type.choices = CONTACT_PREFERENCE_NO_CALLBACK
 
-    @patch(
-        "cla_public.apps.contact.api.get_valid_callback_days",
-        MagicMock(return_value=[datetime.datetime(2024, 1, 1, 0, 0)]),
-    )
-    def test_slots_available(self):
+    @patch("cla_public.apps.contact.forms.ContactForm")
+    def test_slots_available(self, ContactForm):
         current_app.config["USE_BACKEND_CALLBACK_SLOTS"] = True
         form = ContactForm()
         form.contact_type.choices = CONTACT_PREFERENCE
