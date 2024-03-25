66807ca4e27c3253b8b3b4f1f5af11f5690e4005
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-03-25 22:45:09 +0000

    Added additional unit test for not including today

--- cla_public/apps/contact/tests/test_api.py
+++ cla_public/apps/contact/tests/test_api.py
@@ -6,9 +6,10 @@ from cla_public.apps.contact.api import get_valid_callback_days, get_valid_callb
 
 class TestGetValidCallbackDays(TestCase):
     def test_full_week(self):
-        full_week_of_datetimes = [datetime.datetime(2024, 1, day, 9, 0) for day in range(1, 7)]
+        full_week_of_datetimes = [datetime.datetime(2024, 1, day, 9, 0) for day in range(1, 8)]
         with patch("cla_public.apps.contact.api.get_valid_callback_slots", return_value=full_week_of_datetimes):
-            valid_days = get_valid_callback_days()
+            valid_days = get_valid_callback_days(include_today=True)
+            assert len(valid_days) == 7
             for index, day in enumerate(valid_days):
                 assert day.date() == full_week_of_datetimes[index].date()
 
@@ -24,6 +25,12 @@ class TestGetValidCallbackDays(TestCase):
             valid_days = get_valid_callback_days()
         for index, day in enumerate(valid_days):
             assert day.date() == valid_slots[index].date()
+
+    def test_not_include_today(self):
+        full_week_of_datetimes = [datetime.datetime(2024, 1, day, 9, 0) for day in range(1, 7)]
+        with patch("cla_public.apps.contact.api.get_valid_callback_slots", return_value=full_week_of_datetimes):
+            valid_days = get_valid_callback_days(include_today=False)
+            assert len(valid_days) == 6
 
 
 class TestGetValidCallbackTimeslotsOnDate(TestCase):
