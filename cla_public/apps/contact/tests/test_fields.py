4798571370093344877bb1566c1f296fec1d0056
Ben Millar <ben.millar@digital.justice.gov.uk>
2024-04-12 16:48:38 +0100

    Updated unit test

--- cla_public/apps/contact/tests/test_fields.py
+++ cla_public/apps/contact/tests/test_fields.py
@@ -1,32 +1,26 @@
 from unittest import TestCase
 from flask import current_app
 from mock import patch, Mock
-from cla_public.apps.contact.fields import (
-    AvailabilityCheckerForm,
-    ThirdPartyAvailabilityCheckerForm,
-    time_slots_for_day,
-)
+from cla_public.apps.contact.fields import AvailabilityCheckerForm, ThirdPartyAvailabilityCheckerForm
 import datetime
 
 
 class TestFeatureFlag(TestCase):
-    @patch("cla_public.apps.contact.fields.get_valid_callback_timeslots_on_date")
-    @patch("cla_public.apps.contact.fields.OpeningHours.time_slots")
-    def test_feature_flag_enabled(self, cla_backend_slots, cla_common_slots):
+    @patch("cla_public.apps.contact.api.CallCentreCapacity")
+    def test_feature_flag_enabled(self, call_centre_capacity):
         current_app.config["USE_BACKEND_CALLBACK_SLOTS"] = True
-        date = datetime.date(2024, 1, 1)
-        time_slots_for_day(date)
-        cla_backend_slots.assert_called()
-        cla_common_slots.assert_not_called()
+        form = AvailabilityCheckerForm()
+        form.day.capacity = call_centre_capacity
+        form.day.populate_field()
+        call_centre_capacity.get_valid_callback_days.assert_called()
 
-    @patch("cla_public.apps.contact.fields.get_valid_callback_timeslots_on_date")
-    @patch("cla_public.apps.contact.fields.OpeningHours.time_slots")
-    def test_feature_flag_disabled(self, cla_backend_slots, cla_common_slots):
+    @patch("cla_public.apps.contact.api.CallCentreCapacity")
+    def test_feature_flag_disabled(self, call_centre_capacity):
         current_app.config["USE_BACKEND_CALLBACK_SLOTS"] = False
-        date = datetime.date(2024, 1, 1)
-        time_slots_for_day(date)
-        cla_backend_slots.assert_not_called()
-        cla_common_slots.assert_called()
+        form = AvailabilityCheckerForm()
+        form.day.capacity = call_centre_capacity
+        form.day.populate_field()
+        call_centre_capacity.get_valid_callback_days.assert_not_called()
 
 
 class TestCallbackForm(TestCase):
