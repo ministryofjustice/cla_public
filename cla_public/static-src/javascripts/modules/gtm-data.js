56429f71867e5507a9131170acf7e3e68640ec53
digitalali-moj <javid.ali@digital.justice.gov.uk>
2024-07-12 17:56:16 +0100

    LGA-2997 WIP Add FALA search event

--- cla_public/static-src/javascripts/modules/gtm-data.js
+++ cla_public/static-src/javascripts/modules/gtm-data.js
@@ -1,6 +1,6 @@
 'use strict';
 
-moj.Modules.GMTData = {
+moj.Modules.GTMData = {
 
   init: function() {
 
@@ -9,10 +9,10 @@ moj.Modules.GMTData = {
 
     // Track element clicks and form element changes
     $('input, select, button, textarea').on('click change', function(e){
-      
+
       var clickInputTypes = ['checkbox', 'radio', 'button', 'textarea'];
       var thisIsAClickInput = clickInputTypes.includes($(this).attr('type'));
-      
+
       // Don't track changes for clickable input types, only clicks
       if(e.type === 'change' && thisIsAClickInput)return;
 
@@ -28,13 +28,44 @@ moj.Modules.GMTData = {
 
       value = value.trim().replace(/\n/g,'').substring(0,30); // can be up to 100 if needed
 
-      GTM.push({ 
-        'event': 'element-' + e.type, 
-        'element_tag': elem, 
-        'element_id': $(this).attr('id'), 
+      GTM.push({
+        'event': 'element-' + e.type,
+        'element_tag': elem,
+        'element_id': $(this).attr('id'),
         'element_value': value,
-        'element_checked': $(this).is(':checked')     
+        'element_checked': $(this).is(':checked')
       });
     });
-  }
+
+    // Track FALA-lite input
+    this.trackFALA();
+  },
+
+  trackFALA: function() {
+    var url = window.location;
+    if(url.pathname !== '/scope/refer/legal-adviser')return;
+    var urlParams = new URLSearchParams(url.search);
+    var postcode = urlParams.get('postcode');
+    var category = urlParams.get('category');
+    if(!postcode || !category)return;
+
+    var match = postcode.match(/^(.+?)(\d[A-Z]{2})$/i);
+    var district = match ? match[1] : '';
+
+    var mileage = typeof CLOSEST_PROVIDER_MILEAGE === 'string' ? !isNaN(CLOSEST_PROVIDER_MILEAGE) ? CLOSEST_PROVIDER_MILEAGE : '' : '';
+
+    var GTM = window.dataLayer;
+    GTM.push({
+      'event': 'fala_search',
+      'district': district,
+      'category': category,
+      'closest_provider': mileage
+    });
+
+    console.log({
+      'event': 'fala_search',
+      'district': district,
+      'category': category,
+      'closest_provider': mileage
+    });
 }
