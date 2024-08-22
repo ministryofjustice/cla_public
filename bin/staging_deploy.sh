a77d90a2223c123ff3c6defbd90499e094eae59c
digitalali-moj <javid.ali@digital.justice.gov.uk>
2024-08-22 14:30:18 +0100

    Add comments

--- bin/staging_deploy.sh
+++ bin/staging_deploy.sh
@@ -4,6 +4,8 @@ set -e
 ROOT=$(dirname "$0")
 HELM_DIR="$ROOT/../helm_deploy/cla-public/"
 
+# Pull ranges from shared LAA IP ranges and then remove spaces,
+# replace linebreaks with commas, remove last comma, and escape commas for helm input 
 SHARED_IP_RANGES_LAA=$(curl -s https://raw.githubusercontent.com/ministryofjustice/laa-ip-allowlist/main/cidrs.txt | tr -d ' ' | tr '\n' ',' | sed 's/,/\\,/g' | sed 's/\\,$//')
 
 helm upgrade cla-public \
