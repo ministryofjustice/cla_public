3013e16d61a386446f76b7b4fc4aac22cff2fa88
digitalali-moj <javid.ali@digital.justice.gov.uk>
2025-01-15 08:36:15 +0000

    LGA-3471 New temporary env for testing

---
+++ bin/dnstest_deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -e
+
+ROOT=$(dirname "$0")
+HELM_DIR="$ROOT/../helm_deploy/cla-public/"
+
+helm upgrade cla-public \
+  $HELM_DIR \
+  --namespace=laa-cla-public-dnstest \
+  --values ${HELM_DIR}/values-dnstest.yaml \
+  --set host=dnstest.checklegalaid.service.gov.uk \
+  --set ingress.cluster.name=green \
+  --set ingress.cluster.weight=100 \
+  --set image.repository=$DOCKER_REPOSITORY \
+  --set image.tag=$HARDCODED_IMAGE_TAG_IN_CONTEXT \
+  --set-string pingdomIPs=$PINGDOM_IPS \
+  --set-string sharedIPRangesLAA="Any IP allowed but keep this string" \
+  --force \
+  --install
